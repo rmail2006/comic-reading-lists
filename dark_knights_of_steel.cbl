02d092a313f33927067aeeb31a3b6ade2bb5d620
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-07-06 13:53:59 +0100

    Add Dark Knights of Steel complete saga XML
    Added a complete saga list for 'Dark Knights of Steel' including all volumes and series.

---
+++ dark_knights_of_steel.cbl
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ComicList xmlns:xsi="http://w3.org" xmlns:xsd="http://w3.org">
+  <Name>Dark Knights of Steel Complete Saga</Name>
+  <Books>
+    <Book Series="Dark Knights of Steel" Number="1" Volume="2021" />
+    <Book Series="Dark Knights of Steel" Number="2" Volume="2021" />
+    <Book Series="Dark Knights of Steel" Number="3" Volume="2021" />
+    <Book Series="Dark Knights of Steel" Number="4" Volume="2021" />
+    <Book Series="Dark Knights of Steel" Number="5" Volume="2021" />
+    <Book Series="Dark Knights of Steel" Number="6" Volume="2021" />
+    <Book Series="Dark Knights of Steel: Tales from the Three Kingdoms" Number="1" Volume="2022" />
+    <Book Series="Dark Knights of Steel" Number="7" Volume="2021" />
+    <Book Series="Dark Knights of Steel" Number="8" Volume="2021" />
+    <Book Series="Dark Knights of Steel" Number="9" Volume="2021" />
+    <Book Series="Dark Knights of Steel" Number="10" Volume="2021" />
+    <Book Series="Dark Knights of Steel" Number="11" Volume="2021" />
+    <Book Series="Dark Knights of Steel" Number="12" Volume="2021" />
+    <Book Series="Dark Knights of Steel: Allwinter" Number="1" Volume="2024" />
+    <Book Series="Dark Knights of Steel: Allwinter" Number="2" Volume="2024" />
+    <Book Series="Dark Knights of Steel: Allwinter" Number="3" Volume="2024" />
+    <Book Series="Dark Knights of Steel: Allwinter" Number="4" Volume="2024" />
+    <Book Series="Dark Knights of Steel: Allwinter" Number="5" Volume="2024" />
+    <Book Series="Dark Knights of Steel: Allwinter" Number="6" Volume="2024" />
+    <Book Series="Dark Knights of Steel II Special: Heir to the Sea" Number="1" Volume="2026" />
+    <Book Series="Dark Knights of Steel II" Number="1" Volume="2026" />
+    <Book Series="Dark Knights of Steel II" Number="2" Volume="2026" />
+    <Book Series="Dark Knights of Steel II" Number="3" Volume="2026" />
+    <Book Series="Dark Knights of Steel II" Number="4" Volume="2026" />
+    <Book Series="Dark Knights of Steel II" Number="5" Volume="2026" />
+    <Book Series="Dark Knights of Steel II" Number="6" Volume="2026" />
+    <Book Series="Dark Knights of Steel II" Number="7" Volume="2026" />
+    <Book Series="Dark Knights of Steel II" Number="8" Volume="2026" />
+    <Book Series="Dark Knights of Steel II" Number="9" Volume="2026" />
+    <Book Series="Dark Knights of Steel II" Number="10" Volume="2026" />
+    <Book Series="Dark Knights of Steel II" Number="11" Volume="2026" />
+    <Book Series="Dark Knights of Steel II" Number="12" Volume="2026" />
+  </Books>
+</ComicList>
