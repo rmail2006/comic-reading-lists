92415626572684cfebaf6f432f5b3e80d58b6da9
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-07-06 17:17:00 +0100

    Create massive_verse.cbl

---
+++ massive_verse.cbl
@@ -0,0 +1,117 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList xmlns:xsi="http://w3.org" xmlns:xsd="http://w3.org">
+  <Name>The Massive-Verse Complete Reading Order</Name>
+  <Books>
+    <!-- ==================== YEAR ONE & TWO CORE ==================== -->
+    <Book Series="Radiant Black" Number="1" Volume="2021" />
+    <Book Series="Radiant Black" Number="2" Volume="2021" />
+    <Book Series="Radiant Black" Number="3" Volume="2021" />
+    <Book Series="Radiant Black" Number="4" Volume="2021" />
+    <Book Series="Radiant Black" Number="5" Volume="2021" />
+    <Book Series="Radiant Black" Number="6" Volume="2021" />
+    <Book Series="Radiant Black" Number="7" Volume="2021" />
+    <Book Series="Radiant Black" Number="8" Volume="2021" />
+    <Book Series="Radiant Black" Number="9" Volume="2021" />
+    <Book Series="Radiant Black" Number="10" Volume="2021" />
+    <Book Series="Radiant Black" Number="11" Volume="2021" />
+    <Book Series="Radiant Black" Number="12" Volume="2021" />
+    <Book Series="Inferno Girl Red" Number="1" Volume="2023" />
+    <Book Series="Supermassive" Number="1" Volume="2022" />
+    <Book Series="Rogue Sun" Number="1" Volume="2022" />
+    <Book Series="Rogue Sun" Number="2" Volume="2022" />
+    <Book Series="Rogue Sun" Number="3" Volume="2022" />
+    <Book Series="Rogue Sun" Number="4" Volume="2022" />
+    <Book Series="Rogue Sun" Number="5" Volume="2022" />
+    <Book Series="Rogue Sun" Number="6" Volume="2022" />
+    <Book Series="Radiant Black" Number="13" Volume="2021" />
+    <Book Series="Radiant Black" Number="14" Volume="2021" />
+    <Book Series="Radiant Black" Number="15" Volume="2021" />
+    <Book Series="Radiant Red" Number="1" Volume="2022" />
+    <Book Series="Radiant Red" Number="2" Volume="2022" />
+    <Book Series="Radiant Red" Number="3" Volume="2022" />
+    <Book Series="Radiant Red" Number="4" Volume="2022" />
+    <Book Series="Radiant Red" Number="5" Volume="2022" />
+    <Book Series="Radiant Black" Number="16" Volume="2021" />
+    <Book Series="Radiant Black" Number="17" Volume="2021" />
+    <Book Series="Radiant Black" Number="18" Volume="2021" />
+    <Book Series="The Dead Lucky" Number="1" Volume="2022" />
+    <Book Series="The Dead Lucky" Number="2" Volume="2022" />
+    <Book Series="The Dead Lucky" Number="3" Volume="2022" />
+    <Book Series="The Dead Lucky" Number="4" Volume="2022" />
+    <Book Series="The Dead Lucky" Number="5" Volume="2022" />
+    <Book Series="The Dead Lucky" Number="6" Volume="2022" />
+    <Book Series="Rogue Sun" Number="7" Volume="2022" />
+    <Book Series="Rogue Sun" Number="8" Volume="2022" />
+    <Book Series="Rogue Sun" Number="9" Volume="2022" />
+    <Book Series="Rogue Sun" Number="10" Volume="2022" />
+    <Book Series="Rogue Sun" Number="11" Volume="2022" />
+    <Book Series="Rogue Sun" Number="12" Volume="2022" />
+    <Book Series="Radiant Black" Number="19" Volume="2021" />
+    <Book Series="Radiant Black" Number="20" Volume="2021" />
+    <Book Series="Radiant Black" Number="21" Volume="2021" />
+    <Book Series="Radiant Pink" Number="1" Volume="2022" />
+    <Book Series="Radiant Pink" Number="2" Volume="2022" />
+    <Book Series="Radiant Pink" Number="3" Volume="2022" />
+    <Book Series="Radiant Pink" Number="4" Volume="2022" />
+    <Book Series="Radiant Pink" Number="5" Volume="2022" />
+    <Book Series="Radiant Black" Number="22" Volume="2021" />
+    <Book Series="Radiant Black" Number="23" Volume="2021" />
+    <Book Series="Radiant Black" Number="24" Volume="2021" />
+    <Book Series="Supermassive" Number="1" Volume="2023" />
+
+    <!-- ==================== YEAR THREE & FOUR MODERN CONTINUITY ==================== -->
+    <Book Series="The Dead Lucky" Number="7" Volume="2022" />
+    <Book Series="The Dead Lucky" Number="8" Volume="2022" />
+    <Book Series="The Dead Lucky" Number="9" Volume="2022" />
+    <Book Series="The Dead Lucky" Number="10" Volume="2022" />
+    <Book Series="The Dead Lucky" Number="11" Volume="2022" />
+    <Book Series="The Dead Lucky" Number="12" Volume="2022" />
+    <Book Series="Rogue Sun" Number="13" Volume="2022" />
+    <Book Series="Rogue Sun" Number="14" Volume="2022" />
+    <Book Series="Rogue Sun" Number="15" Volume="2022" />
+    <Book Series="Rogue Sun" Number="16" Volume="2022" />
+    <Book Series="Rogue Sun" Number="17" Volume="2022" />
+    <Book Series="Rogue Sun" Number="18" Volume="2022" />
+    <Book Series="No/One" Number="1" Volume="2023" />
+    <Book Series="No/One" Number="2" Volume="2023" />
+    <Book Series="No/One" Number="3" Volume="2023" />
+    <Book Series="No/One" Number="4" Volume="2023" />
+    <Book Series="No/One" Number="5" Volume="2023" />
+    <Book Series="No/One" Number="6" Volume="2023" />
+    <Book Series="No/One" Number="7" Volume="2023" />
+    <Book Series="No/One" Number="8" Volume="2023" />
+    <Book Series="No/One" Number="9" Volume="2023" />
+    <Book Series="No/One" Number="10" Volume="2023" />
+    
+    <!-- The Catalyst War Era (Parallel Timelines combined) -->
+    <Book Series="Radiant Black" Number="25" Volume="2021" />
+    <Book Series="Radiant Black" Number="26" Volume="2021" />
+    <Book Series="Radiant Black" Number="27" Volume="2021" />
+    <Book Series="Radiant Black" Number="28" Volume="2021" />
+    <Book Series="Radiant Black" Number="29" Volume="2021" />
+    <Book Series="Radiant Black" Number="30" Volume="2021" />
+    <Book Series="Supermassive" Number="1" Volume="2024" />
+    <Book Series="Rogue Sun" Number="19" Volume="2022" />
+    <Book Series="Rogue Sun" Number="20" Volume="2022" />
+    <Book Series="Rogue Sun" Number="21" Volume="2022" />
+    <Book Series="Rogue Sun" Number="22" Volume="2022" />
+    <Book Series="Rogue Sun" Number="23" Volume="2022" />
+    <Book Series="Rogue Sun" Number="24" Volume="2022" />
+
+    <!-- Modern Era (2025–2026 Launches) -->
+    <Book Series="Supermassive" Number="1" Volume="2025" />
+    <Book Series="Radiant Black" Number="31" Volume="2021" />
+    <Book Series="Radiant Black" Number="32" Volume="2021" />
+    <Book Series="Radiant Black" Number="33" Volume="2021" />
+    <Book Series="Radiant Black" Number="34" Volume="2021" />
+    <Book Series="Radiant Black" Number="35" Volume="2021" />
+    <Book Series="Radiant Black" Number="36" Volume="2021" />
+    <Book Series="Radiant Black" Number="37" Volume="2021" />
+    <Book Series="Radiant Black" Number="38" Volume="2021" />
+    <Book Series="Radiant Black" Number="39" Volume="2021" />
+    <Book Series="Radiant Black" Number="40" Volume="2021" />
+    <Book Series="Radiant Black" Number="41" Volume="2021" />
+    <Book Series="Radiant Black" Number="42" Volume="2021" />
+    <Book Series="Radiant Black" Number="43" Volume="2021" />
+  </Books>
+</ReadingList>
