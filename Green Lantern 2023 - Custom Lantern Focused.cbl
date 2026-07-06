c55264d3e870b98120dc7860fe5856dd99958679
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-07-06 10:08:41 +0100

    Add files via upload

---
+++ Green Lantern 2023 - Custom Lantern Focused.cbl
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList Name="Green Lantern 2023 - Custom Lantern Focused">
+  <Books>
+    <Book Series="Green Lantern" Volume="2023" Number="1" Year="2023" />
+    <Book Series="Green Lantern" Volume="2023" Number="2" Year="2023" />
+    <Book Series="Dawn of DC Knight Terrors FCBD Special Edition" Volume="2023" Number="1" Year="2023" />
+    <Book Series="Knight Terrors: First Blood" Volume="2023" Number="1" Year="2023" />
+    <Book Series="Knight Terrors" Volume="2023" Number="1" Year="2023" />
+    <Book Series="Knight Terrors: Green Lantern" Volume="2023" Number="1" Year="2023" />
+    <Book Series="Knight Terrors" Volume="2023" Number="2" Year="2023" />
+    <Book Series="Knight Terrors: Green Lantern" Volume="2023" Number="2" Year="2023" />
+    <Book Series="Knight Terrors" Volume="2023" Number="3" Year="2023" />
+    <Book Series="Knight Terrors" Volume="2023" Number="4" Year="2023" />
+    <Book Series="Knight Terrors: Night's End" Volume="2023" Number="1" Year="2023" />
+    <Book Series="Green Lantern" Volume="2023" Number="3" Year="2023" />
+    <Book Series="Green Lantern" Volume="2023" Number="4" Year="2023" />
+    <Book Series="Green Lantern" Volume="2023" Number="5" Year="2023" />
+    <Book Series="Green Lantern: War Journal" Volume="2023" Number="1" Year="2023" />
+    <Book Series="Green Lantern" Volume="2023" Number="6" Year="2024" />
+    <Book Series="Green Lantern: War Journal" Volume="2023" Number="2" Year="2023" />
+    <Book Series="Green Lantern: War Journal" Volume="2023" Number="3" Year="2024" />
+    <Book Series="Green Lantern: War Journal" Volume="2023" Number="4" Year="2024" />
+    <Book Series="Green Lantern: War Journal" Volume="2023" Number="5" Year="2024" />
+    <Book Series="Green Lantern: War Journal" Volume="2023" Number="6" Year="2024" />
+    <Book Series="Green Lantern" Volume="2023" Number="7" Year="2024" />
+    <Book Series="Green Lantern" Volume="2023" Number="8" Year="2024" />
+    <Book Series="Green Lantern" Volume="2023" Number="9" Year="2024" />
+    <Book Series="Green Lantern" Volume="2023" Number="10" Year="2024" />
+    <Book Series="Green Lantern" Volume="2023" Number="11" Year="2024" />
+    <Book Series="Green Lantern" Volume="2023" Number="12" Year="2024" />
+    <Book Series="Green Lantern: War Journal" Volume="2023" Number="7" Year="2024" />
+    <Book Series="Green Lantern: War Journal" Volume="2023" Number="8" Year="2024" />
+    <Book Series="Green Lantern: War Journal" Volume="2023" Number="9" Year="2024" />
+    <Book Series="Green Lantern: War Journal" Volume="2023" Number="10" Year="2024" />
+    <Book Series="Green Lantern: War Journal" Volume="2023" Number="11" Year="2024" />
+    <Book Series="Green Lantern: War Journal" Volume="2023" Number="12" Year="2024" />
+    <Book Series="Absolute Power: Ground Zero" Volume="2024" Number="1" Year="2024" />
+    <Book Series="Absolute Power" Volume="2024" Number="1" Year="2024" />
+    <Book Series="Green Lantern" Volume="2023" Number="13" Year="2024" />
+    <Book Series="Absolute Power: Task Force VII" Volume="2024" Number="3" Year="2024" />
+    <Book Series="Absolute Power" Volume="2024" Number="2" Year="2024" />
+    <Book Series="Green Lantern" Volume="2023" Number="14" Year="2024" />
+    <Book Series="Absolute Power" Volume="2024" Number="3" Year="2024" />
+    <Book Series="Green Lantern" Volume="2023" Number="15" Year="2024" />
+    <Book Series="Absolute Power" Volume="2024" Number="4" Year="2024" />
+    <Book Series="DC All In Special" Volume="2024" Number="1" Year="2024" />
+    <Book Series="Green Lantern: Civil Corps Special" Volume="2024" Number="1" Year="2024" />
+    <Book Series="Green Lantern" Volume="2023" Number="16" Year="2024" />
+    <Book Series="Green Lantern" Volume="2023" Number="17" Year="2025" />
+    <Book Series="Green Lantern" Volume="2023" Number="18" Year="2025" />
+    <Book Series="Green Lantern: Fractured Spectrum Special" Volume="2025" Number="1" Year="2025" />
+    <Book Series="Green Lantern" Volume="2023" Number="19" Year="2025" />
+    <Book Series="Green Lantern" Volume="2023" Number="20" Year="2025" />
+    <Book Series="Green Lantern" Volume="2023" Number="21" Year="2025" />
+    <Book Series="Green Lantern" Volume="2023" Number="22" Year="2025" />
+    <Book Series="Green Lantern" Volume="2023" Number="23" Year="2025" />
+    <Book Series="Green Lantern" Volume="2023" Number="24" Year="2025" />
+    <Book Series="Green Lantern Corps" Volume="2025" Number="1" Year="2025" />
+    <Book Series="Green Lantern Corps" Volume="2025" Number="2" Year="2025" />
+    <Book Series="Green Lantern Corps" Volume="2025" Number="3" Year="2025" />
+    <Book Series="Green Lantern Corps" Volume="2025" Number="4" Year="2025" />
+    <Book Series="Green Lantern Corps" Volume="2025" Number="5" Year="2025" />
+    <Book Series="Green Lantern Corps" Volume="2025" Number="6" Year="2025" />
+    <Book Series="Green Lantern" Volume="2023" Number="25" Year="2025" />
+    <Book Series="Green Lantern Corps" Volume="2025" Number="7" Year="2025" />
+    <Book Series="Green Lantern" Volume="2023" Number="26" Year="2025" />
+    <Book Series="Green Lantern Corps" Volume="2025" Number="8" Year="2025" />
+    <Book Series="Green Lantern" Volume="2023" Number="27" Year="2025" />
+    <Book Series="Green Lantern Corps" Volume="2025" Number="9" Year="2025" />
+	<Book Series="Green Lantern" Volume="2023" Number="28" Year="2025" />
+	<Book Series="Green Lantern Corps" Volume="2025" Number="10" Year="2025" />
+	<Book Series="Green Lantern" Volume="2023" Number="29" Year="2025" />
+	<Book Series="Green Lantern Corps" Volume="2025" Number="11" Year="2025" />
+  </Books>
+</ReadingList>
