021504041c07fed185707f435c267ad75a50612e
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-07-06 13:52:15 +0100

    Create DCeased complete reading order XML
    Added a complete reading order for the DCeased comic series, including all relevant volumes and issues.

---
+++ dceased_complete.cbl
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ComicList xmlns:xsi="http://w3.org" xmlns:xsd="http://w3.org">
+  <Name>DCeased Complete Reading Order</Name>
+  <Books>
+    <Book Series="DCeased" Number="1" Volume="2019" />
+    <Book Series="DCeased" Number="2" Volume="2019" />
+    <Book Series="DCeased" Number="3" Volume="2019" />
+    <Book Series="DCeased" Number="4" Volume="2019" />
+    <Book Series="DCeased: A Good Day to Die" Number="1" Volume="2019" />
+    <Book Series="DCeased" Number="5" Volume="2019" />
+    <Book Series="DCeased: Hope at World's End" Number="1" Volume="2020" />
+    <Book Series="DCeased: Hope at World's End" Number="2" Volume="2020" />
+    <Book Series="DCeased: Hope at World's End" Number="3" Volume="2020" />
+    <Book Series="DCeased: Hope at World's End" Number="4" Volume="2020" />
+    <Book Series="DCeased: Hope at World's End" Number="5" Volume="2020" />
+    <Book Series="DCeased: Hope at World's End" Number="6" Volume="2020" />
+    <Book Series="DCeased: Hope at World's End" Number="7" Volume="2020" />
+    <Book Series="DCeased: Hope at World's End" Number="8" Volume="2020" />
+    <Book Series="DCeased: Hope at World's End" Number="9" Volume="2020" />
+    <Book Series="DCeased: Hope at World's End" Number="10" Volume="2020" />
+    <Book Series="DCeased: Hope at World's End" Number="11" Volume="2020" />
+    <Book Series="DCeased: Hope at World's End" Number="12" Volume="2020" />
+    <Book Series="DCeased: Hope at World's End" Number="13" Volume="2020" />
+    <Book Series="DCeased: Hope at World's End" Number="14" Volume="2020" />
+    <Book Series="DCeased: Hope at World's End" Number="15" Volume="2020" />
+    <Book Series="DCeased" Number="6" Volume="2019" />
+    <Book Series="DCeased: Unkillables" Number="1" Volume="2020" />
+    <Book Series="DCeased: Unkillables" Number="2" Volume="2020" />
+    <Book Series="DCeased: Unkillables" Number="3" Volume="2020" />
+    <Book Series="DCeased: Dead Planet" Number="1" Volume="2020" />
+    <Book Series="DCeased: Dead Planet" Number="2" Volume="2020" />
+    <Book Series="DCeased: Dead Planet" Number="3" Volume="2020" />
+    <Book Series="DCeased: Dead Planet" Number="4" Volume="2020" />
+    <Book Series="DCeased: Dead Planet" Number="5" Volume="2020" />
+    <Book Series="DCeased: Dead Planet" Number="6" Volume="2020" />
+    <Book Series="DCeased: Dead Planet" Number="7" Volume="2020" />
+    <Book Series="DCeased: War of the Undead Gods" Number="1" Volume="2022" />
+    <Book Series="DCeased: War of the Undead Gods" Number="2" Volume="2022" />
+    <Book Series="DCeased: War of the Undead Gods" Number="3" Volume="2022" />
+    <Book Series="DCeased: War of the Undead Gods" Number="4" Volume="2022" />
+    <Book Series="DCeased: War of the Undead Gods" Number="5" Volume="2022" />
+    <Book Series="DCeased: War of the Undead Gods" Number="6" Volume="2022" />
+    <Book Series="DCeased: War of the Undead Gods" Number="7" Volume="2022" />
+    <Book Series="DCeased: War of the Undead Gods" Number="8" Volume="2022" />
+  </Books>
+</ComicList>
