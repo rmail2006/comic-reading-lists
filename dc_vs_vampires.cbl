4b856d7f242a427d121df9c5129f3a1af004665d
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-07-06 16:19:32 +0100

    Create dc_vs_vampires.cbl

---
+++ dc_vs_vampires.cbl
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList xmlns:xsi="http://w3.org" xmlns:xsd="http://w3.org">
+  <Name>DC vs. Vampires Complete Saga</Name>
+  <Books>
+    <Book Series="DC vs. Vampires" Number="1" Volume="2021" />
+    <Book Series="DC vs. Vampires" Number="2" Volume="2021" />
+    <Book Series="DC vs. Vampires" Number="3" Volume="2021" />
+    <Book Series="DC vs. Vampires" Number="4" Volume="2021" />
+    <Book Series="DC vs. Vampires" Number="5" Volume="2021" />
+    <Book Series="DC vs. Vampires" Number="6" Volume="2021" />
+    <Book Series="DC vs. Vampires: Hunters" Number="1" Volume="2022" />
+    <Book Series="DC vs. Vampires: Killers" Number="1" Volume="2022" />
+    <Book Series="DC vs. Vampires: Lazarus Injection" Number="1" Volume="2022" />
+    <Book Series="DC vs. Vampires" Number="7" Volume="2021" />
+    <Book Series="DC vs. Vampires: All-Out War" Number="1" Volume="2022" />
+    <Book Series="DC vs. Vampires" Number="8" Volume="2021" />
+    <Book Series="DC vs. Vampires: All-Out War" Number="2" Volume="2022" />
+    <Book Series="DC vs. Vampires" Number="9" Volume="2021" />
+    <Book Series="DC vs. Vampires: All-Out War" Number="3" Volume="2022" />
+    <Book Series="DC vs. Vampires" Number="10" Volume="2021" />
+    <Book Series="DC vs. Vampires: All-Out War" Number="4" Volume="2022" />
+    <Book Series="DC vs. Vampires" Number="11" Volume="2021" />
+    <Book Series="DC vs. Vampires: All-Out War" Number="5" Volume="2022" />
+    <Book Series="DC vs. Vampires: All-Out War" Number="6" Volume="2022" />
+    <Book Series="DC vs. Vampires" Number="12" Volume="2021" />
+    <Book Series="DC vs. Vampires: World War V" Number="1" Volume="2024" />
+    <Book Series="DC vs. Vampires: World War V" Number="2" Volume="2024" />
+    <Book Series="DC vs. Vampires: World War V" Number="3" Volume="2024" />
+    <Book Series="DC vs. Vampires: World War V" Number="4" Volume="2024" />
+    <Book Series="DC vs. Vampires: World War V" Number="5" Volume="2024" />
+    <Book Series="DC vs. Vampires: World War V" Number="6" Volume="2024" />
+    <Book Series="DC vs. Vampires: World War V - Darkness and Light" Number="1" Volume="2025" />
+    <Book Series="DC vs. Vampires: World War V" Number="7" Volume="2025" />
+    <Book Series="DC vs. Vampires: World War V" Number="8" Volume="2025" />
+    <Book Series="DC vs. Vampires: World War V" Number="9" Volume="2025" />
+    <Book Series="DC vs. Vampires: World War V" Number="10" Volume="2025" />
+    <Book Series="DC vs. Vampires: World War V" Number="11" Volume="2025" />
+    <Book Series="DC vs. Vampires: World War V" Number="12" Volume="2025" />
+  </Books>
+</ReadingList>
