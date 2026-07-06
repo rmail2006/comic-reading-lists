16c8810e68d9350ec0c3589aaf5cefcc3c0d61b3
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-07-06 13:53:42 +0100

    Add Marvel Zombies core timeline XML data

---
+++ marvel_zombies.cbl
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ComicList xmlns:xsi="http://w3.org" xmlns:xsd="http://w3.org">
+  <Name>Marvel Zombies Core Timeline</Name>
+  <Books>
+    <Book Series="Ultimate Fantastic Four" Number="21" Volume="2003" />
+    <Book Series="Ultimate Fantastic Four" Number="22" Volume="2003" />
+    <Book Series="Ultimate Fantastic Four" Number="23" Volume="2003" />
+    <Book Series="Marvel Zombies: Dead Days" Number="1" Volume="2007" />
+    <Book Series="Ultimate Fantastic Four" Number="30" Volume="2003" />
+    <Book Series="Ultimate Fantastic Four" Number="31" Volume="2003" />
+    <Book Series="Ultimate Fantastic Four" Number="32" Volume="2003" />
+    <Book Series="Marvel Zombies" Number="1" Volume="2006" />
+    <Book Series="Marvel Zombies" Number="2" Volume="2006" />
+    <Book Series="Marvel Zombies" Number="3" Volume="2006" />
+    <Book Series="Marvel Zombies" Number="4" Volume="2006" />
+    <Book Series="Marvel Zombies" Number="5" Volume="2006" />
+    <Book Series="Marvel Zombies vs. Army of Darkness" Number="1" Volume="2007" />
+    <Book Series="Marvel Zombies vs. Army of Darkness" Number="2" Volume="2007" />
+    <Book Series="Marvel Zombies vs. Army of Darkness" Number="3" Volume="2007" />
+    <Book Series="Marvel Zombies vs. Army of Darkness" Number="4" Volume="2007" />
+    <Book Series="Marvel Zombies vs. Army of Darkness" Number="5" Volume="2007" />
+    <Book Series="Black Panther" Number="28" Volume="2005" />
+    <Book Series="Black Panther" Number="29" Volume="2005" />
+    <Book Series="Black Panther" Number="30" Volume="2005" />
+    <Book Series="Marvel Zombies 2" Number="1" Volume="2007" />
+    <Book Series="Marvel Zombies 2" Number="2" Volume="2007" />
+    <Book Series="Marvel Zombies 2" Number="3" Volume="2007" />
+    <Book Series="Marvel Zombies 2" Number="4" Volume="2007" />
+    <Book Series="Marvel Zombies 2" Number="5" Volume="2007" />
+    <Book Series="Marvel Zombies 3" Number="1" Volume="2008" />
+    <Book Series="Marvel Zombies 3" Number="2" Volume="2008" />
+    <Book Series="Marvel Zombies 3" Number="3" Volume="2008" />
+    <Book Series="Marvel Zombies 3" Number="4" Volume="2008" />
+    <Book Series="Marvel Zombies 4" Number="1" Volume="2009" />
+    <Book Series="Marvel Zombies 4" Number="2" Volume="2009" />
+    <Book Series="Marvel Zombies 4" Number="3" Volume="2009" />
+    <Book Series="Marvel Zombies 4" Number="4" Volume="2009" />
+    <Book Series="Marvel Zombies Return" Number="1" Volume="2009" />
+    <Book Series="Marvel Zombies Return" Number="2" Volume="2009" />
+    <Book Series="Marvel Zombies Return" Number="3" Volume="2009" />
+    <Book Series="Marvel Zombies Return" Number="4" Volume="2009" />
+    <Book Series="Marvel Zombies Return" Number="5" Volume="2009" />
+  </Books>
+</ComicList>
