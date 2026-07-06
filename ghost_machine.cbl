b549e20d3ce65fb2101a7a001c6c4f62265b1a95
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-07-06 22:18:51 +0100

    Add XML reading list for Ghost Machine series

---
+++ ghost_machine.cbl
@@ -0,0 +1,115 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList xmlns:xsi="http://w3.org" xmlns:xsd="http://w3.org">
+  <Name>Ghost Machine Imprint Reading Order</Name>
+  <Books>
+    <!-- ==================== PHASE 1: THE UNNAMED ORIGINS ==================== -->
+    <Book Series="Geiger" Number="1" Volume="2021" />
+    <Book Series="Geiger" Number="2" Volume="2021" />
+    <Book Series="Geiger" Number="3" Volume="2021" />
+    <Book Series="Geiger" Number="4" Volume="2021" />
+    <Book Series="Geiger" Number="5" Volume="2021" />
+    <Book Series="Geiger" Number="6" Volume="2021" />
+    <Book Series="Geiger 80-Page Giant" Number="1" Volume="2022" />
+    <Book Series="Junkyard Joe" Number="1" Volume="2022" />
+    <Book Series="Junkyard Joe" Number="2" Volume="2022" />
+    <Book Series="Junkyard Joe" Number="3" Volume="2022" />
+    <Book Series="Junkyard Joe" Number="4" Volume="2022" />
+    <Book Series="Junkyard Joe" Volume="2022" Number="5" />
+    <Book Series="Junkyard Joe" Number="6" Volume="2022" />
+    <Book Series="Geiger: Ground Zero" Number="1" Volume="2023" />
+    <Book Series="Geiger: Ground Zero" Number="2" Volume="2023" />
+    <Book Series="Ghost Machine" Number="1" Volume="2024" />
+
+    <!-- ==================== PHASE 2: CURRENT EXPANSION RUNS ==================== -->
+    <!-- The Unnamed Universe Ongoing Block -->
+    <Book Series="Geiger" Number="1" Volume="2024" />
+    <Book Series="Redcoat" Number="1" Volume="2024" />
+    <Book Series="Geiger" Number="2" Volume="2024" />
+    <Book Series="Redcoat" Number="2" Volume="2024" />
+    <Book Series="Geiger" Number="3" Volume="2024" />
+    <Book Series="Redcoat" Number="3" Volume="2024" />
+    <Book Series="Geiger" Number="4" Volume="2024" />
+    <Book Series="Redcoat" Number="4" Volume="2024" />
+    <Book Series="Geiger" Number="5" Volume="2024" />
+    <Book Series="Redcoat" Number="5" Volume="2024" />
+    <Book Series="Geiger" Number="6" Volume="2024" />
+    <Book Series="Redcoat" Number="6" Volume="2024" />
+    <Book Series="Geiger" Number="7" Volume="2024" />
+    <Book Series="Redcoat" Number="7" Volume="2024" />
+    <Book Series="Geiger" Number="8" Volume="2024" />
+    <Book Series="Redcoat" Number="8" Volume="2024" />
+    <Book Series="Geiger" Number="9" Volume="2024" />
+    <Book Series="Redcoat" Number="9" Volume="2024" />
+    <Book Series="Geiger" Number="10" Volume="2024" />
+    <Book Series="Redcoat" Number="10" Volume="2024" />
+    <Book Series="Geiger" Number="11" Volume="2024" />
+    <Book Series="Redcoat" Number="11" Volume="2024" />
+    <Book Series="Geiger" Number="12" Volume="2024" />
+    <Book Series="Redcoat" Number="12" Volume="2024" />
+    <Book Series="Geiger" Number="13" Volume="2024" />
+    <Book Series="Redcoat" Number="13" Volume="2024" />
+    <Book Series="Geiger" Number="14" Volume="2024" />
+    <Book Series="Redcoat" Number="14" Volume="2024" />
+    <Book Series="Geiger" Number="15" Volume="2024" />
+    <Book Series="Redcoat" Number="15" Volume="2024" />
+    <Book Series="Geiger" Number="16" Volume="2024" />
+    <Book Series="Redcoat" Number="16" Volume="2024" />
+    <Book Series="Geiger" Number="17" Volume="2024" />
+    <Book Series="Redcoat" Number="17" Volume="2024" />
+    <Book Series="Geiger" Number="18" Volume="2024" />
+    <Book Series="Redcoat" Number="18" Volume="2024" />
+    <Book Series="Geiger" Number="19" Volume="2024" />
+    <Book Series="Geiger" Number="20" Volume="2024" />
+    <Book Series="Geiger" Number="21" Volume="2024" />
+    <Book Series="Geiger" Number="22" Volume="2024" />
+    <Book Series="Geiger" Number="23" Volume="2024" />
+    
+    <!-- Sci-Fi Universe Block -->
+    <Book Series="Rook: Exodus" Number="1" Volume="2024" />
+    <Book Series="Rook: Exodus" Number="2" Volume="2024" />
+    <Book Series="Rook: Exodus" Number="3" Volume="2024" />
+    <Book Series="Rook: Exodus" Number="4" Volume="2024" />
+    <Book Series="Rook: Exodus" Number="5" Volume="2024" />
+    <Book Series="Rook: Exodus" Number="6" Volume="2024" />
+    <Book Series="Rook: Exodus" Number="7" Volume="2024" />
+    <Book Series="Rook: Exodus" Number="8" Volume="2024" />
+    <Book Series="Rook: Exodus" Number="9" Volume="2024" />
+    <Book Series="Rook: Exodus" Number="10" Volume="2024" />
+
+    <!-- Family Odyssey Universe Block -->
+    <Book Series="The Rocketfellers" Number="0" Volume="2024" />
+    <Book Series="The Rocketfellers" Number="1" Volume="2024" />
+    <Book Series="The Rocketfellers" Number="2" Volume="2024" />
+    <Book Series="The Rocketfellers" Number="3" Volume="2024" />
+    <Book Series="The Rocketfellers" Number="4" Volume="2024" />
+    <Book Series="The Rocketfellers" Number="5" Volume="2024" />
+    <Book Series="The Rocketfellers" Number="6" Volume="2024" />
+    <Book Series="The Rocketfellers" Number="7" Volume="2024" />
+    <Book Series="The Rocketfellers" Number="8" Volume="2024" />
+    <Book Series="The Rocketfellers" Number="9" Volume="2024" />
+    <Book Series="The Rocketfellers" Number="10" Volume="2024" />
+    <Book Series="The Rocketfellers" Number="11" Volume="2024" />
+    <Book Series="The Rocketfellers" Number="12" Volume="2024" />
+    <Book Series="The Rocketfellers" Number="13" Volume="2024" />
+    
+    <!-- Horror Universe Block (Hyde Street Core) -->
+    <Book Series="It Happened on Hyde Street: Devour" Number="1" Volume="2024" />
+    <Book Series="Hyde Street" Number="1" Volume="2024" />
+    <Book Series="Hyde Street" Number="2" Volume="2024" />
+    <Book Series="Hyde Street" Number="3" Volume="2024" />
+    <Book Series="Hyde Street" Number="4" Volume="2024" />
+    <Book Series="Hyde Street" Number="5" Volume="2024" />
+    <Book Series="Hyde Street" Number="6" Volume="2024" />
+    <Book Series="Hyde Street" Number="7" Volume="2024" />
+    <Book Series="Hyde Street" Number="8" Volume="2024" />
+    <Book Series="Hyde Street" Number="9" Volume="2024" />
+    <Book Series="Hyde Street" Number="10" Volume="2024" />
+    <Book Series="Hyde Street" Number="11" Volume="2024" />
+    <Book Series="Hyde Street" Number="12" Volume="2024" />
+    <Book Series="Sisterhood: A Hyde Street Story" Number="1" Volume="2025" />
+    <Book Series="Sisterhood: A Hyde Street Story" Number="2" Volume="2025" />
+    <Book Series="Sisterhood: A Hyde Street Story" Number="3" Volume="2025" />
+    <Book Series="Sisterhood: A Hyde Street Story" Number="4" Volume="2025" />
+    <Book Series="Sisterhood: A Hyde Street Story" Number="5" Volume="2025" />
+  </Books>
+</ReadingList>
