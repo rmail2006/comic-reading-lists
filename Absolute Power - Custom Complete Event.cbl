0f97b7c9949680dd429f0d60fb20940cd55a8fd5
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-07-06 10:11:43 +0100

    Add files via upload

---
+++ Absolute Power - Custom Complete Event.cbl
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList Name="Absolute Power - Custom Complete Event">
+  <Books>
+    <Book Series="Absolute Power 2024 FCBD Special Edition" Volume="2024" Number="1" Year="2024" />
+    <Book Series="Absolute Power: Ground Zero" Volume="2024" Number="1" Year="2024" />
+    <Book Series="Absolute Power" Volume="2024" Number="1" Year="2024" />
+    <Book Series="Wonder Woman" Volume="2023" Number="11" Year="2024" />
+    <Book Series="Absolute Power: Task Force VII" Volume="2024" Number="1" Year="2024" />
+    <Book Series="Batman" Volume="2016" Number="150" Year="2024" />
+    <Book Series="Superman" Volume="2023" Number="16" Year="2024" />
+    <Book Series="Green Arrow" Volume="2023" Number="14" Year="2024" />
+    <Book Series="Green Lantern" Volume="2023" Number="13" Year="2024" />
+    <Book Series="Absolute Power: Task Force VII" Volume="2024" Number="2" Year="2024" />
+    <Book Series="Absolute Power: Task Force VII" Volume="2024" Number="3" Year="2024" />
+    <Book Series="Absolute Power" Volume="2024" Number="2" Year="2024" />
+    <Book Series="Green Lantern" Volume="2023" Number="14" Year="2024" />
+    <Book Series="Green Arrow" Volume="2023" Number="15" Year="2024" />
+    <Book Series="Wonder Woman" Volume="2023" Number="12" Year="2024" />
+    <Book Series="Batman" Volume="2016" Number="151" Year="2024" />
+    <Book Series="Batman" Volume="2016" Number="152" Year="2024" />
+    <Book Series="Green Arrow" Volume="2023" Number="16" Year="2024" />
+    <Book Series="Superman" Volume="2023" Number="17" Year="2024" />
+    <Book Series="Superman" Volume="2023" Number="18" Year="2024" />
+    <Book Series="Absolute Power: Task Force VII" Volume="2024" Number="4" Year="2024" />
+    <Book Series="Absolute Power: Task Force VII" Volume="2024" Number="5" Year="2024" />
+    <Book Series="Absolute Power" Volume="2024" Number="3" Year="2024" />
+    <Book Series="Green Lantern" Volume="2023" Number="15" Year="2024" />
+    <Book Series="Absolute Power: Super Son" Volume="2024" Number="1" Year="2024" />
+    <Book Series="Absolute Power: Task Force VII" Volume="2024" Number="6" Year="2024" />
+    <Book Series="Wonder Woman" Volume="2023" Number="13" Year="2024" />
+    <Book Series="Absolute Power: Task Force VII" Volume="2024" Number="7" Year="2024" />
+    <Book Series="Absolute Power: Origins" Volume="2024" Number="1" Year="2024" />
+    <Book Series="Absolute Power: Origins" Volume="2024" Number="2" Year="2024" />
+    <Book Series="Absolute Power: Origins" Volume="2024" Number="3" Year="2024" />
+    <Book Series="Absolute Power" Volume="2024" Number="4" Year="2024" />
+  </Books>
+</ReadingList>
---
+++ Road to Absolute Power - Custom.cbl
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList Name="Road to Absolute Power - Custom">
+  <Books>
+    <Book Series="Batman" Volume="2016" Number="145" Year="2024" />
+    <Book Series="Batman" Volume="2016" Number="146" Year="2024" />
+    <Book Series="Batman" Volume="2016" Number="147" Year="2024" />
+    <Book Series="Suicide Squad: Dream Team" Volume="2024" Number="1" Year="2024" />
+    <Book Series="Suicide Squad: Dream Team" Volume="2024" Number="2" Year="2024" />
+    <Book Series="Suicide Squad: Dream Team" Volume="2024" Number="3" Year="2024" />
+    <Book Series="Green Arrow" Volume="2023" Number="12" Year="2024" />
+    <Book Series="Suicide Squad: Dream Team" Volume="2024" Number="4" Year="2024" />
+    <Book Series="Wonder Woman" Volume="2023" Number="10" Year="2024" />
+    <Book Series="Batman" Volume="2016" Number="148" Year="2024" />
+    <Book Series="Batman" Volume="2016" Number="149" Year="2024" />
+    <Book Series="Superman" Volume="2023" Number="13" Year="2024" />
+    <Book Series="Action Comics" Volume="2016" Number="1064" Year="2024" />
+    <Book Series="Superman" Volume="2023" Number="14" Year="2024" />
+    <Book Series="Action Comics" Volume="2016" Number="1065" Year="2024" />
+    <Book Series="Superman: House of Brainiac Special" Volume="2024" Number="1" Year="2024" />
+    <Book Series="Superman" Volume="2023" Number="15" Year="2024" />
+    <Book Series="Action Comics" Volume="2016" Number="1066" Year="2024" />
+  </Books>
+</ReadingList>
