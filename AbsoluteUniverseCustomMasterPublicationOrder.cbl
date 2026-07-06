648699dafcec4ca10a3553eadd6d6b2ab96fd754
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-07-06 10:53:41 +0100

    Create AbsoluteUniverseCustomMasterPublicationOrder.cbl

---
+++ AbsoluteUniverseCustomMasterPublicationOrder.cbl
@@ -0,0 +1,114 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList Name="Absolute Universe - Custom Master Publication Order">
+  <Books>
+    <Book Series="DC All In Special" Volume="2024" Number="1" Year="2024" />
+    <Book Series="Absolute Batman" Volume="2024" Number="1" Year="2024" />
+    <Book Series="Absolute Batman" Volume="2024" Number="2" Year="2024" />
+    <Book Series="Absolute Batman" Volume="2024" Number="3" Year="2024" />
+    <Book Series="Absolute Batman" Volume="2024" Number="4" Year="2024" />
+    <Book Series="Absolute Batman" Volume="2024" Number="5" Year="2024" />
+    <Book Series="Absolute Batman" Volume="2024" Number="6" Year="2024" />
+    <Book Series="Absolute Superman" Volume="2024" Number="1" Year="2024" />
+    <Book Series="Absolute Superman" Volume="2024" Number="2" Year="2024" />
+    <Book Series="Absolute Superman" Volume="2024" Number="3" Year="2024" />
+    <Book Series="Absolute Superman" Volume="2024" Number="4" Year="2024" />
+    <Book Series="Absolute Superman" Volume="2024" Number="5" Year="2024" />
+    <Book Series="Absolute Superman" Volume="2024" Number="6" Year="2024" />
+    <Book Series="Absolute Wonder Woman" Volume="2024" Number="1" Year="2024" />
+    <Book Series="Absolute Wonder Woman" Volume="2024" Number="2" Year="2024" />
+    <Book Series="Absolute Wonder Woman" Volume="2024" Number="3" Year="2024" />
+    <Book Series="Absolute Wonder Woman" Volume="2024" Number="4" Year="2024" />
+    <Book Series="Absolute Wonder Woman" Volume="2024" Number="5" Year="2024" />
+    <Book Series="Absolute Wonder Woman" Volume="2024" Number="6" Year="2024" />
+    <Book Series="Absolute Wonder Woman" Volume="2024" Number="7" Year="2024" />
+    <Book Series="Absolute Batman" Volume="2024" Number="7" Year="2025" />
+    <Book Series="Absolute Batman" Volume="2024" Number="8" Year="2025" />
+    <Book Series="Absolute Flash" Volume="2025" Number="1" Year="2025" />
+    <Book Series="Absolute Flash" Volume="2025" Number="2" Year="2025" />
+    <Book Series="Absolute Flash" Volume="2025" Number="3" Year="2025" />
+    <Book Series="Absolute Martian Manhunter" Volume="2025" Number="1" Year="2025" />
+    <Book Series="Absolute Martian Manhunter" Volume="2025" Number="2" Year="2025" />
+    <Book Series="Absolute Green Lantern" Volume="2025" Number="1" Year="2025" />
+    <Book Series="Absolute Green Lantern" Volume="2025" Number="2" Year="2025" />
+    <Book Series="Absolute Green Lantern" Volume="2025" Number="3" Year="2025" />
+    <Book Series="Absolute Green Lantern" Volume="2025" Number="4" Year="2025" />
+    <Book Series="Absolute Green Lantern" Volume="2025" Number="5" Year="2025" />
+    <Book Series="Absolute Green Lantern" Volume="2025" Number="6" Year="2025" />
+    <Book Series="Absolute Martian Manhunter" Volume="2025" Number="3" Year="2025" />
+    <Book Series="Absolute Martian Manhunter" Volume="2025" Number="4" Year="2025" />
+    <Book Series="Absolute Flash" Volume="2025" Number="4" Year="2025" />
+    <Book Series="Absolute Flash" Volume="2025" Number="5" Year="2025" />
+    <Book Series="Absolute Flash" Volume="2025" Number="6" Year="2025" />
+    <Book Series="Absolute Flash" Volume="2025" Number="7" Year="2025" />
+    <Book Series="Absolute Martian Manhunter" Volume="2025" Number="5" Year="2025" />
+    <Book Series="Absolute Martian Manhunter" Volume="2025" Number="6" Year="2025" />
+    <Book Series="Absolute Batman" Volume="2024" Number="9" Year="2025" />
+    <Book Series="Absolute Batman" Volume="2024" Number="10" Year="2025" />
+    <Book Series="Absolute Batman" Volume="2024" Number="11" Year="2025" />
+    <Book Series="Absolute Batman" Volume="2024" Number="12" Year="2025" />
+    <Book Series="Absolute Batman" Volume="2024" Number="13" Year="2025" />
+    <Book Series="Absolute Batman" Volume="2024" Number="14" Year="2025" />
+    <Book Series="Absolute Wonder Woman" Volume="2024" Number="8" Year="2025" />
+    <Book Series="Absolute Wonder Woman" Volume="2024" Number="9" Year="2025" />
+    <Book Series="Absolute Wonder Woman" Volume="2024" Number="10" Year="2025" />
+    <Book Series="Absolute Wonder Woman" Volume="2024" Number="11" Year="2025" />
+    <Book Series="Absolute Wonder Woman" Volume="2024" Number="12" Year="2025" />
+    <Book Series="Absolute Superman" Volume="2024" Number="7" Year="2025" />
+    <Book Series="Absolute Superman" Volume="2024" Number="8" Year="2025" />
+    <Book Series="Absolute Superman" Volume="2024" Number="9" Year="2025" />
+    <Book Series="Absolute Superman" Volume="2024" Number="10" Year="2025" />
+    <Book Series="Absolute Superman" Volume="2024" Number="11" Year="2025" />
+    <Book Series="Free Comic Book Day 2025: DC All In Special Edition" Volume="2025" Number="1" Year="2025" />
+    <Book Series="Absolute Evil" Volume="2025" Number="1" Year="2025" />
+    <Book Series="Absolute Batman 2025 Annual" Volume="2025" Number="1" Year="2025" />
+    <Book Series="Absolute Green Lantern" Volume="2025" Number="7" Year="2025" />
+    <Book Series="Absolute Green Lantern" Volume="2025" Number="8" Year="2025" />
+    <Book Series="Absolute Green Lantern" Volume="2025" Number="9" Year="2025" />
+    <Book Series="Absolute Green Lantern" Volume="2025" Number="10" Year="2025" />
+    <Book Series="Absolute Flash" Volume="2025" Number="8" Year="2025" />
+    <Book Series="Absolute Flash" Volume="2025" Number="9" Year="2025" />
+    <Book Series="Absolute Flash" Volume="2025" Number="10" Year="2025" />
+    <Book Series="Absolute Flash" Volume="2025" Number="11" Year="2025" />
+    <Book Series="Absolute Flash" Volume="2025" Number="12" Year="2025" />
+    <Book Series="Absolute Wonder Woman" Volume="2024" Number="13" Year="2025" />
+    <Book Series="Absolute Wonder Woman" Volume="2024" Number="14" Year="2025" />
+    <Book Series="Absolute Superman" Volume="2024" Number="12" Year="2025" />
+    <Book Series="Absolute Superman" Volume="2024" Number="13" Year="2025" />
+    <Book Series="Absolute Superman" Volume="2024" Number="14" Year="2025" />
+    <Book Series="Absolute Batman" Volume="2024" Number="15" Year="2025" />
+    <Book Series="Absolute Batman: Ark M" Volume="2026" Number="1" Year="2026" />
+    <Book Series="Absolute Wonder Woman" Volume="2024" Number="15" Year="2025" />
+    <Book Series="Absolute Batman" Volume="2024" Number="16" Year="2026" />
+    <Book Series="Absolute Martian Manhunter" Volume="2025" Number="7" Year="2025" />
+    <Book Series="Absolute Martian Manhunter" Volume="2025" Number="8" Year="2025" />
+    <Book Series="Absolute Wonder Woman 2026 Annual" Volume="2026" Number="1" Year="2026" />
+    <Book Series="Absolute Superman" Volume="2024" Number="15" Year="2026" />
+    <Book Series="Absolute Superman" Volume="2024" Number="16" Year="2026" />
+    <Book Series="Absolute Superman" Volume="2024" Number="17" Year="2026" />
+    <Book Series="Absolute Martian Manhunter" Volume="2025" Number="9" Year="2026" />
+    <Book Series="Absolute Martian Manhunter" Volume="2025" Number="10" Year="2026" />
+    <Book Series="Absolute Wonder Woman" Volume="2024" Number="16" Year="2026" />
+    <Book Series="Absolute Wonder Woman" Volume="2024" Number="17" Year="2026" />
+    <Book Series="Absolute Wonder Woman" Volume="2024" Number="18" Year="2026" />
+    <Book Series="Absolute Wonder Woman" Volume="2024" Number="19" Year="2026" />
+    <Book Series="Absolute Wonder Woman" Volume="2024" Number="20" Year="2026" />
+    <Book Series="Absolute Green Lantern" Volume="2025" Number="11" Year="2026" />
+    <Book Series="Absolute Green Lantern" Volume="2025" Number="12" Year="2026" />
+    <Book Series="Absolute Green Lantern" Volume="2025" Number="13" Year="2026" />
+    <Book Series="Absolute Green Lantern" Volume="2025" Number="14" Year="2026" />
+    <Book Series="Absolute Green Lantern" Volume="2025" Number="15" Year="2026" />
+    <Book Series="Absolute Batman" Volume="2024" Number="17" Year="2026" />
+    <Book Series="Absolute Batman" Volume="2024" Number="18" Year="2026" />
+    <Book Series="Absolute Batman" Volume="2024" Number="19" Year="2026" />
+    <Book Series="Absolute Batman" Volume="2024" Number="20" Year="2026" />
+    <Book Series="Absolute Flash" Volume="2025" Number="13" Year="2026" />
+    <Book Series="Absolute Flash" Volume="2025" Number="14" Year="2026" />
+    <Book Series="Absolute Flash" Volume="2025" Number="15" Year="2026" />
+    <Book Series="Absolute Superman" Volume="2024" Number="18" Year="2026" />
+    <Book Series="Absolute Superman" Volume="2024" Number="19" Year="2026" />
+    <Book Series="Absolute Martian Manhunter" Volume="2025" Number="11" Year="2026" />
+    <Book Series="Absolute Green Arrow" Volume="2026" Number="1" Year="2026" />
+    <Book Series="Absolute Green Arrow" Volume="2026" Number="2" Year="2026" />
+    <Book Series="Absolute Catwoman" Volume="2026" Number="1" Year="2026" />
+  </Books>
+</ReadingList>
