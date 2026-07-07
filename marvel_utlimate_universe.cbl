64cc45be877f0c6d414d0cb539d68ed722e0f2f3
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-07-07 14:49:49 +0100

    Add additional Ultimate Spider-Man books to list

---
+++ marvel_utlimate_universe.cbl
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList xmlns:xsi="http://w3.org" xmlns:xsd="http://w3.org">
+  <Name>Marvel: The Ultimate Universe Rebirth</Name>
+  <Books>
+    <!-- ==================== PHASE 1: THE MULTIVERSE COLLAPSE ==================== -->
+    <!-- The Event That Wiped Out the Original Timelines -->
+    <Book Series="Secret Wars" Number="1" Volume="2015" />
+    <Book Series="Secret Wars" Number="2" Volume="2015" />
+    <Book Series="Secret Wars" Number="3" Volume="2015" />
+    <Book Series="Secret Wars" Number="4" Volume="2015" />
+    <Book Series="Secret Wars" Number="5" Volume="2015" />
+    <Book Series="Secret Wars" Number="6" Volume="2015" />
+    <Book Series="Secret Wars" Number="7" Volume="2015" />
+    <Book Series="Secret Wars" Number="8" Volume="2015" />
+    <Book Series="Secret Wars" Number="9" Volume="2015" />
+
+    <!-- ==================== PHASE 2: THE ALTERNATE TIMELINE BRANCH ==================== -->
+    <!-- The Anniversary Special "What If" Speculative Sequel to 2015 -->
+    <Book Series="What If...? Secret Wars" Number="1" Volume="2026" />
+
+    <!-- ==================== PHASE 3: THE NEW CHRONOLOGY BRIDGE ==================== -->
+    <!-- The Maker Escapes and Rewrites Earth history -->
+    <Book Series="Ultimate Invasion" Number="1" Volume="2023" />
+    <Book Series="Ultimate Invasion" Number="2" Volume="2023" />
+    <Book Series="Ultimate Invasion" Number="3" Volume="2023" />
+    <Book Series="Ultimate Invasion" Number="4" Volume="2023" />
+    <Book Series="Ultimate Universe" Number="1" Volume="2023" />
+
+    <!-- ==================== PHASE 4: THE NEW ULTIMATE REBORN REALITY ==================== -->
+    <!-- The Modern Ongoing Continuation Following an Adult Peter Parker -->
+    <Book Series="Ultimate Spider-Man" Number="1" Volume="2024" />
+    <Book Series="Ultimate Spider-Man" Number="2" Volume="2024" />
+    <Book Series="Ultimate Spider-Man" Number="3" Volume="2024" />
+    <Book Series="Ultimate Spider-Man" Number="4" Volume="2024" />
+    <Book Series="Ultimate Spider-Man" Number="5" Volume="2024" />
+    <Book Series="Ultimate Spider-Man" Number="6" Volume="2024" />
+    <Book Series="Ultimate Spider-Man" Number="7" Volume="2024" />
+    <Book Series="Ultimate Spider-Man" Number="8" Volume="2024" />
+    <Book Series="Ultimate Spider-Man" Number="9" Volume="2024" />
+    <Book Series="Ultimate Spider-Man" Number="10" Volume="2024" />
+    <Book Series="Ultimate Spider-Man" Number="11" Volume="2024" />
+    <Book Series="Ultimate Spider-Man" Number="12" Volume="2024" />
+    <Book Series="Ultimate Spider-Man" Number="13" Volume="2024" />
+    <Book Series="Ultimate Spider-Man" Number="14" Volume="2024" />
+    <Book Series="Ultimate Spider-Man" Number="15" Volume="2024" />
+    <Book Series="Ultimate Spider-Man" Number="16" Volume="2024" />
+    <Book Series="Ultimate Spider-Man" Number="17" Volume="2024" />
+    <Book Series="Ultimate Spider-Man" Number="18" Volume="2024" />
+    <Book Series="Ultimate Spider-Man" Number="19" Volume="2024" />
+    <Book Series="Ultimate Spider-Man" Number="20" Volume="2024" />
+    <Book Series="Ultimate Spider-Man" Number="21" Volume="2024" />
+    <Book Series="Ultimate Spider-Man" Number="22" Volume="2024" />
+    <Book Series="Ultimate Spider-Man" Number="23" Volume="2024" />
+    <Book Series="Ultimate Spider-Man" Number="24" Volume="2024" />
+  </Books>
+</ReadingList>
