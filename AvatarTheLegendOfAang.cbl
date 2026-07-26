be595386e4e158ff7b47c4952b04444bb8bea927
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-07-26 23:46:53 +0100

    Update book entries with 'Avatar - ' prefix

--- AvatarTheLegendOfAang.cbl
+++ AvatarTheLegendOfAang.cbl
@@ -2,38 +2,38 @@
 <ReadingList Name="Omnibus - Avatar The Last Airbender - Custom Reading Order">
   <Name>Omnibus - Avatar The Last Airbender</Name>
   <Books>
-    <Book Series="The Lost Adventures" Volume="2011" Number="1" Year="2011" />
-    <Book Series="Team Avatar Tales" Volume="2019" Number="1" Year="2019" />
-    <Book Series="Katara and the Pirate's Silver" Volume="2020" Number="1" Year="2020" />
-    <Book Series="Suki Alone" Volume="2021" Number="1" Year="2021" />
-    <Book Series="Aang's Unfreezing Day" Volume="2023" Number="1" Year="2023" />
-    <Book Series="Sokka's Boomerang Class" Volume="2026" Number="1" Year="2026" />
-    <Book Series="The Promise" Volume="2012" Number="1" Year="2012" />
-    <Book Series="The Promise" Volume="2012" Number="2" Year="2012" />
-    <Book Series="The Promise" Volume="2012" Number="3" Year="2012" />
-    <Book Series="The Search" Volume="2013" Number="1" Year="2013" />
-    <Book Series="The Search" Volume="2013" Number="2" Year="2013" />
-    <Book Series="The Search" Volume="2013" Number="3" Year="2013" />
-    <Book Series="The Rift" Volume="2014" Number="1" Year="2014" />
-    <Book Series="The Rift" Volume="2014" Number="2" Year="2014" />
-    <Book Series="The Rift" Volume="2014" Number="3" Year="2014" />
-    <Book Series="Toph Beifong's Metalbending Academy" Volume="2021" Number="1" Year="2021" />
-    <Book Series="Smoke and Shadow" Volume="2015" Number="1" Year="2015" />
-    <Book Series="Smoke and Shadow" Volume="2015" Number="2" Year="2015" />
-    <Book Series="Smoke and Shadow" Volume="2015" Number="3" Year="2016" />
-    <Book Series="North and South" Volume="2016" Number="1" Year="2016" />
-    <Book Series="North and South" Volume="2016" Number="2" Year="2017" />
-    <Book Series="North and South" Volume="2016" Number="3" Year="2017" />
-    <Book Series="Azula in the Spirit Temple" Volume="2023" Number="1" Year="2023" />
-    <Book Series="The Bounty Hunter and the Tea Brewer" Volume="2024" Number="1" Year="2024" />
-    <Book Series="Ashes of the Academy" Volume="2025" Number="1" Year="2025" />
-    <Book Series="Imbalance" Volume="2018" Number="1" Year="2018" />
-    <Book Series="Imbalance" Volume="2018" Number="2" Year="2019" />
-    <Book Series="Imbalance" Volume="2018" Number="3" Year="2019" />
-    <Book Series="The Kyoshi Warriors" Volume="2026" Number="1" Year="2026" />
-    <Book Series="The Kyoshi Warriors" Volume="2026" Number="2" Year="2026" />
-    <Book Series="The Kyoshi Warriors" Volume="2026" Number="3" Year="2026" />
-    <Book Series="Avatar Legends - Masters of the Elements" Volume="2026" Number="1" Year="2026" />
-    <Book Series="Jet - Rebels and Rhinos" Volume="2026" Number="1" Year="2026" />
+    <Book Series="Avatar - The Lost Adventures" Volume="2011" Number="1" Year="2011" />
+    <Book Series="Avatar - Team Avatar Tales" Volume="2019" Number="1" Year="2019" />
+    <Book Series="Avatar - Katara and the Pirate's Silver" Volume="2020" Number="1" Year="2020" />
+    <Book Series="Avatar - Suki Alone" Volume="2021" Number="1" Year="2021" />
+    <Book Series="Avatar - Aang's Unfreezing Day" Volume="2023" Number="1" Year="2023" />
+    <Book Series="Avatar - Sokka's Boomerang Class" Volume="2026" Number="1" Year="2026" />
+    <Book Series="Avatar - The Promise" Volume="2012" Number="1" Year="2012" />
+    <Book Series="Avatar - The Promise" Volume="2012" Number="2" Year="2012" />
+    <Book Series="Avatar - The Promise" Volume="2012" Number="3" Year="2012" />
+    <Book Series="Avatar - The Search" Volume="2013" Number="1" Year="2013" />
+    <Book Series="Avatar - The Search" Volume="2013" Number="2" Year="2013" />
+    <Book Series="Avatar - The Search" Volume="2013" Number="3" Year="2013" />
+    <Book Series="Avatar - The Rift" Volume="2014" Number="1" Year="2014" />
+    <Book Series="Avatar - The Rift" Volume="2014" Number="2" Year="2014" />
+    <Book Series="Avatar - The Rift" Volume="2014" Number="3" Year="2014" />
+    <Book Series="Avatar - Toph Beifong's Metalbending Academy" Volume="2021" Number="1" Year="2021" />
+    <Book Series="Avatar - Smoke and Shadow" Volume="2015" Number="1" Year="2015" />
+    <Book Series="Avatar - Smoke and Shadow" Volume="2015" Number="2" Year="2015" />
+    <Book Series="Avatar - Smoke and Shadow" Volume="2015" Number="3" Year="2016" />
+    <Book Series="Avatar - North and South" Volume="2016" Number="1" Year="2016" />
+    <Book Series="Avatar - North and South" Volume="2016" Number="2" Year="2017" />
+    <Book Series="Avatar - North and South" Volume="2016" Number="3" Year="2017" />
+    <Book Series="Avatar - Azula in the Spirit Temple" Volume="2023" Number="1" Year="2023" />
+    <Book Series="Avatar - The Bounty Hunter and the Tea Brewer" Volume="2024" Number="1" Year="2024" />
+    <Book Series="Avatar - Ashes of the Academy" Volume="2025" Number="1" Year="2025" />
+    <Book Series="Avatar - Imbalance" Volume="2018" Number="1" Year="2018" />
+    <Book Series="Avatar - Imbalance" Volume="2018" Number="2" Year="2019" />
+    <Book Series="Avatar - Imbalance" Volume="2018" Number="3" Year="2019" />
+    <Book Series="Avatar - The Kyoshi Warriors" Volume="2026" Number="1" Year="2026" />
+    <Book Series="Avatar - The Kyoshi Warriors" Volume="2026" Number="2" Year="2026" />
+    <Book Series="Avatar - The Kyoshi Warriors" Volume="2026" Number="3" Year="2026" />
+    <Book Series="Avatar - Avatar Legends - Masters of the Elements" Volume="2026" Number="1" Year="2026" />
+    <Book Series="Avatar - Jet - Rebels and Rhinos" Volume="2026" Number="1" Year="2026" />
   </Books>
 </ReadingList>
