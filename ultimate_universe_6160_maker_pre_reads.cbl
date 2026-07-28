9de738c401e5e487d2d3f0f25ae58609ef0ca5d3
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-07-28 23:54:39 +0100

    Add XML reading list for Ultimate Universe 6160

---
+++ ultimate_universe_6160_maker_pre_reads.cbl
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList Name="Omnibus - Ultimate Universe 6160 - Maker Pre-Reads">
+  <Name>Omnibus - Ultimate Universe 6160 - Maker Pre-Reads</Name>
+  <Books>
+    <Book Series="Ultimate Enemy" Volume="2010" Number="1" Year="2010" />
+    <Book Series="Ultimate Enemy" Volume="2010" Number="2" Year="2010" />
+    <Book Series="Ultimate Enemy" Volume="2010" Number="3" Year="2010" />
+    <Book Series="Ultimate Enemy" Volume="2010" Number="4" Year="2010" />
+    <Book Series="Ultimate Mystery" Volume="2010" Number="1" Year="2010" />
+    <Book Series="Ultimate Mystery" Volume="2010" Number="2" Year="2010" />
+    <Book Series="Ultimate Mystery" Volume="2010" Number="3" Year="2010" />
+    <Book Series="Ultimate Mystery" Volume="2010" Number="4" Year="2010" />
+    <Book Series="Ultimate Doom" Volume="2011" Number="1" Year="2011" />
+    <Book Series="Ultimate Doom" Volume="2011" Number="2" Year="2011" />
+    <Book Series="Ultimate Doom" Volume="2011" Number="3" Year="2011" />
+    <Book Series="Ultimate Doom" Volume="2011" Number="4" Year="2011" />
+    <Book Series="Ultimate Comics Ultimates" Volume="2011" Number="1" Year="2011" />
+    <Book Series="Ultimate Comics Ultimates" Volume="2011" Number="2" Year="2011" />
+    <Book Series="Ultimate Comics Ultimates" Volume="2011" Number="3" Year="2011" />
+    <Book Series="Ultimate Comics Ultimates" Volume="2011" Number="4" Year="2011" />
+    <Book Series="Ultimate Comics Ultimates" Volume="2011" Number="5" Year="2011" />
+    <Book Series="Ultimate Comics Ultimates" Volume="2011" Number="6" Year="2012" />
+    <Book Series="Ultimate Comics Ultimates" Volume="2011" Number="7" Year="2012" />
+    <Book Series="Ultimate Comics Ultimates" Volume="2011" Number="8" Year="2012" />
+    <Book Series="Ultimate Comics Ultimates" Volume="2011" Number="9" Year="2012" />
+    <Book Series="Ultimate Comics Ultimates" Volume="2011" Number="10" Year="2012" />
+    <Book Series="Ultimate Comics Ultimates" Volume="2011" Number="11" Year="2012" />
+    <Book Series="Ultimate Comics Ultimates" Volume="2011" Number="12" Year="2012" />
+    <Book Series="Ultimate Comics Ultimates" Volume="2011" Number="25" Year="2013" />
+    <Book Series="Ultimate Comics Ultimates" Volume="2011" Number="26" Year="2013" />
+    <Book Series="Ultimate Comics Ultimates" Volume="2011" Number="27" Year="2013" />
+    <Book Series="Ultimate Comics Ultimates" Volume="2011" Number="28" Year="2013" />
+    <Book Series="Ultimate Comics Ultimates" Volume="2011" Number="29" Year="2013" />
+    <Book Series="Ultimate Comics Ultimates" Volume="2011" Number="30" Year="2013" />
+    <Book Series="Cataclysm The Ultimates Last Stand" Volume="2013" Number="1" Year="2013" />
+    <Book Series="Cataclysm The Ultimates Last Stand" Volume="2013" Number="2" Year="2013" />
+    <Book Series="Cataclysm The Ultimates Last Stand" Volume="2013" Number="3" Year="2014" />
+    <Book Series="Cataclysm The Ultimates Last Stand" Volume="2013" Number="4" Year="2014" />
+    <Book Series="Cataclysm The Ultimates Last Stand" Volume="2013" Number="5" Year="2014" />
+    <Book Series="Survive" Volume="2014" Number="1" Year="2014" />
+    <Book Series="Secret Wars" Volume="2015" Number="1" Year="2015" />
+    <Book Series="Secret Wars" Volume="2015" Number="2" Year="2015" />
+    <Book Series="Secret Wars" Volume="2015" Number="3" Year="2015" />
+    <Book Series="Secret Wars" Volume="2015" Number="4" Year="2015" />
+    <Book Series="Secret Wars" Volume="2015" Number="5" Year="2015" />
+    <Book Series="Secret Wars" Volume="2015" Number="6" Year="2015" />
+    <Book Series="Secret Wars" Volume="2015" Number="7" Year="2015" />
+    <Book Series="Secret Wars" Volume="2015" Number="8" Year="2015" />
+    <Book Series="Secret Wars" Volume="2015" Number="9" Year="2015" />
+    <Book Series="Venom" Volume="2018" Number="26" Year="2020" />
+    <Book Series="Venom" Volume="2018" Number="27" Year="2020" />
+    <Book Series="Venom" Volume="2018" Number="28" Year="2020" />
+    <Book Series="Venom" Volume="2018" Number="29" Year="2020" />
+    <Book Series="Venom" Volume="2018" Number="30" Year="2020" />
+  </Books>
+</ReadingList>
