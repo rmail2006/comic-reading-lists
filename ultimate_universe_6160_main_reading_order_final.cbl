f4c15332f50bf7597e663e352fe0f2f3dc8895e9
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-07-29 00:22:42 +0100

    Create Ultimate Universe 6160 main reading order
    Added a comprehensive reading order for the Ultimate Universe 6160, including various series and volumes for multiple years.

---
+++ ultimate_universe_6160_main_reading_order_final.cbl
@@ -0,0 +1,138 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList Name="Event - Ultimate Universe 6160 - Main Reading Order">
+  <Name>Event - Ultimate Universe 6160 - Main Reading Order</Name>
+  <Books>
+    <Book Series="Ultimate Invasion" Volume="2023" Number="1" Year="2023" />
+    <Book Series="Ultimate Invasion" Volume="2023" Number="2" Year="2023" />
+    <Book Series="Ultimate Invasion" Volume="2023" Number="3" Year="2023" />
+    <Book Series="Ultimate Invasion" Volume="2023" Number="4" Year="2023" />
+    <Book Series="Ultimate Universe" Volume="2023" Number="1" Year="2023" />
+    <Book Series="Free Comic Book Day 2024 Spiderman Ultimate Universe" Volume="2024" Number="1" Year="2024" />
+    <Book Series="Ultimate Spider-Man" Volume="2024" Number="1" Year="2024" />
+    <Book Series="Ultimate Spider-Man" Volume="2024" Number="2" Year="2024" />
+    <Book Series="Ultimate Spider-Man" Volume="2024" Number="3" Year="2024" />
+    <Book Series="Ultimate Spider-Man" Volume="2024" Number="4" Year="2024" />
+    <Book Series="Ultimate Spider-Man" Volume="2024" Number="5" Year="2024" />
+    <Book Series="Ultimate Spider-Man" Volume="2024" Number="6" Year="2024" />
+    <Book Series="Ultimate Spider-Man" Volume="2024" Number="7" Year="2024" />
+    <Book Series="Ultimate Spider-Man" Volume="2024" Number="8" Year="2024" />
+    <Book Series="Ultimate Spider-Man" Volume="2024" Number="9" Year="2024" />
+    <Book Series="Ultimate Spider-Man" Volume="2024" Number="10" Year="2024" />
+    <Book Series="Ultimate Spider-Man" Volume="2024" Number="11" Year="2024" />
+    <Book Series="Ultimate Spider-Man" Volume="2024" Number="12" Year="2024" />
+    <Book Series="Ultimate Black Panther" Volume="2024" Number="1" Year="2024" />
+    <Book Series="Ultimate Black Panther" Volume="2024" Number="2" Year="2024" />
+    <Book Series="Ultimate Black Panther" Volume="2024" Number="3" Year="2024" />
+    <Book Series="Ultimate Black Panther" Volume="2024" Number="4" Year="2024" />
+    <Book Series="Ultimate Black Panther" Volume="2024" Number="5" Year="2024" />
+    <Book Series="Ultimate Black Panther" Volume="2024" Number="6" Year="2024" />
+    <Book Series="Ultimate Black Panther" Volume="2024" Number="7" Year="2024" />
+    <Book Series="Ultimate Black Panther" Volume="2024" Number="8" Year="2024" />
+    <Book Series="Ultimate Black Panther" Volume="2024" Number="9" Year="2024" />
+    <Book Series="Ultimate Black Panther" Volume="2024" Number="10" Year="2024" />
+    <Book Series="Ultimate Black Panther" Volume="2024" Number="11" Year="2025" />
+    <Book Series="Ultimate Black Panther" Volume="2024" Number="12" Year="2025" />
+    <Book Series="Ultimate X-Men" Volume="2024" Number="1" Year="2024" />
+    <Book Series="Ultimate X-Men" Volume="2024" Number="2" Year="2024" />
+    <Book Series="Ultimate X-Men" Volume="2024" Number="3" Year="2024" />
+    <Book Series="Ultimate X-Men" Volume="2024" Number="4" Year="2024" />
+    <Book Series="Ultimate X-Men" Volume="2024" Number="5" Year="2024" />
+    <Book Series="Ultimate X-Men" Volume="2024" Number="6" Year="2024" />
+    <Book Series="Ultimate X-Men" Volume="2024" Number="7" Year="2024" />
+    <Book Series="Ultimate X-Men" Volume="2024" Number="8" Year="2024" />
+    <Book Series="Ultimate X-Men" Volume="2024" Number="9" Year="2024" />
+    <Book Series="Ultimate X-Men" Volume="2024" Number="10" Year="2024" />
+    <Book Series="Ultimate X-Men" Volume="2024" Number="11" Year="2025" />
+    <Book Series="Ultimate X-Men" Volume="2024" Number="12" Year="2025" />
+    <Book Series="Ultimates" Volume="2024" Number="1" Year="2024" />
+    <Book Series="Ultimates" Volume="2024" Number="2" Year="2024" />
+    <Book Series="Ultimates" Volume="2024" Number="3" Year="2024" />
+    <Book Series="Ultimates" Volume="2024" Number="4" Year="2024" />
+    <Book Series="Ultimates" Volume="2024" Number="5" Year="2024" />
+    <Book Series="Ultimates" Volume="2024" Number="6" Year="2024" />
+    <Book Series="Ultimate Universe One Year In" Volume="2024" Number="1" Year="2024" />
+    <Book Series="Ultimate Spider-Man" Volume="2024" Number="13" Year="2025" />
+    <Book Series="Ultimate Spider-Man" Volume="2024" Number="14" Year="2025" />
+    <Book Series="Ultimate Spider-Man" Volume="2024" Number="15" Year="2025" />
+    <Book Series="Ultimate Spider-Man" Volume="2024" Number="16" Year="2025" />
+    <Book Series="Ultimate Spider-Man" Volume="2024" Number="17" Year="2025" />
+    <Book Series="Ultimate Spider-Man" Volume="2024" Number="18" Year="2025" />
+    <Book Series="Ultimate Spider-Man" Volume="2024" Number="19" Year="2025" />
+    <Book Series="Ultimate Spider-Man" Volume="2024" Number="20" Year="2025" />
+    <Book Series="Ultimate Spider-Man" Volume="2024" Number="21" Year="2025" />
+    <Book Series="Ultimate Spider-Man" Volume="2024" Number="22" Year="2025" />
+    <Book Series="Ultimate Spider-Man" Volume="2024" Number="23" Year="2025" />
+    <Book Series="Ultimate Spider-Man" Volume="2024" Number="24" Year="2025" />
+    <Book Series="Ultimate Black Panther" Volume="2024" Number="13" Year="2025" />
+    <Book Series="Ultimate Black Panther" Volume="2024" Number="14" Year="2025" />
+    <Book Series="Ultimate Black Panther" Volume="2024" Number="15" Year="2025" />
+    <Book Series="Ultimate Black Panther" Volume="2024" Number="16" Year="2025" />
+    <Book Series="Ultimate Black Panther" Volume="2024" Number="17" Year="2025" />
+    <Book Series="Ultimate Black Panther" Volume="2024" Number="18" Year="2025" />
+    <Book Series="Ultimate Black Panther" Volume="2024" Number="19" Year="2025" />
+    <Book Series="Ultimate Black Panther" Volume="2024" Number="20" Year="2025" />
+    <Book Series="Ultimate Black Panther" Volume="2024" Number="21" Year="2025" />
+    <Book Series="Ultimate Black Panther" Volume="2024" Number="22" Year="2025" />
+    <Book Series="Ultimate Black Panther" Volume="2024" Number="23" Year="2025" />
+    <Book Series="Ultimate Black Panther" Volume="2024" Number="24" Year="2026" />
+    <Book Series="Ultimate X-Men" Volume="2024" Number="13" Year="2025" />
+    <Book Series="Ultimate X-Men" Volume="2024" Number="14" Year="2025" />
+    <Book Series="Ultimate X-Men" Volume="2024" Number="15" Year="2025" />
+    <Book Series="Ultimate X-Men" Volume="2024" Number="16" Year="2025" />
+    <Book Series="Ultimate X-Men" Volume="2024" Number="17" Year="2025" />
+    <Book Series="Ultimate X-Men" Volume="2024" Number="18" Year="2025" />
+    <Book Series="Ultimate X-Men" Volume="2024" Number="19" Year="2025" />
+    <Book Series="Ultimate X-Men" Volume="2024" Number="20" Year="2025" />
+    <Book Series="Ultimate X-Men" Volume="2024" Number="21" Year="2025" />
+    <Book Series="Ultimate X-Men" Volume="2024" Number="22" Year="2025" />
+    <Book Series="Ultimate X-Men" Volume="2024" Number="23" Year="2026" />
+    <Book Series="Ultimate X-Men" Volume="2024" Number="24" Year="2026" />
+    <Book Series="Ultimates" Volume="2024" Number="7" Year="2025" />
+    <Book Series="Ultimates" Volume="2024" Number="8" Year="2025" />
+    <Book Series="Ultimates" Volume="2024" Number="9" Year="2025" />
+    <Book Series="Ultimates" Volume="2024" Number="10" Year="2025" />
+    <Book Series="Ultimates" Volume="2024" Number="11" Year="2025" />
+    <Book Series="Ultimates" Volume="2024" Number="12" Year="2025" />
+    <Book Series="Ultimates" Volume="2024" Number="13" Year="2025" />
+    <Book Series="Ultimates" Volume="2024" Number="14" Year="2025" />
+    <Book Series="Ultimates" Volume="2024" Number="15" Year="2025" />
+    <Book Series="Ultimates" Volume="2024" Number="16" Year="2025" />
+    <Book Series="Ultimates" Volume="2024" Number="17" Year="2025" />
+    <Book Series="Ultimates" Volume="2024" Number="18" Year="2025" />
+    <Book Series="Ultimate Wolverine" Volume="2025" Number="1" Year="2025" />
+    <Book Series="Ultimate Wolverine" Volume="2025" Number="2" Year="2025" />
+    <Book Series="Ultimate Wolverine" Volume="2025" Number="3" Year="2025" />
+    <Book Series="Ultimate Wolverine" Volume="2025" Number="4" Year="2025" />
+    <Book Series="Ultimate Wolverine" Volume="2025" Number="5" Year="2025" />
+    <Book Series="Ultimate Wolverine" Volume="2025" Number="6" Year="2025" />
+    <Book Series="Ultimate Wolverine" Volume="2025" Number="7" Year="2025" />
+    <Book Series="Ultimate Wolverine" Volume="2025" Number="8" Year="2025" />
+    <Book Series="Ultimate Wolverine" Volume="2025" Number="9" Year="2025" />
+    <Book Series="Ultimate Wolverine" Volume="2025" Number="10" Year="2025" />
+    <Book Series="Ultimate Wolverine" Volume="2025" Number="11" Year="2025" />
+    <Book Series="Ultimate Wolverine" Volume="2025" Number="12" Year="2025" />
+    <Book Series="Free Comic Book Day 2025 Amazing Spider-Man Ultimate Universe" Volume="2025" Number="1" Year="2025" />
+    <Book Series="Ultimate Spider-Man Incursion" Volume="2025" Number="1" Year="2025" />
+    <Book Series="Ultimate Spider-Man Incursion" Volume="2025" Number="2" Year="2025" />
+    <Book Series="Ultimate Spider-Man Incursion" Volume="2025" Number="3" Year="2025" />
+    <Book Series="Ultimate Spider-Man Incursion" Volume="2025" Number="4" Year="2025" />
+    <Book Series="Ultimate Spider-Man Incursion" Volume="2025" Number="5" Year="2025" />
+    <Book Series="Ultimate Universe Two Years In" Volume="2025" Number="1" Year="2025" />
+    <Book Series="Ultimates" Volume="2024" Number="19" Year="2025" />
+    <Book Series="Ultimates" Volume="2024" Number="20" Year="2025" />
+    <Book Series="Ultimates" Volume="2024" Number="21" Year="2025" />
+    <Book Series="Ultimates" Volume="2024" Number="22" Year="2026" />
+    <Book Series="Ultimates" Volume="2024" Number="23" Year="2026" />
+    <Book Series="Ultimates" Volume="2024" Number="24" Year="2026" />
+    <Book Series="Ultimate Wolverine" Volume="2025" Number="13" Year="2025" />
+    <Book Series="Ultimate Wolverine" Volume="2025" Number="14" Year="2026" />
+    <Book Series="Ultimate Wolverine" Volume="2025" Number="15" Year="2026" />
+    <Book Series="Ultimate Wolverine" Volume="2025" Number="16" Year="2026" />
+    <Book Series="Ultimate Endgame" Volume="2025" Number="1" Year="2025" />
+    <Book Series="Ultimate Endgame" Volume="2025" Number="2" Year="2025" />
+    <Book Series="Ultimate Endgame" Volume="2025" Number="3" Year="2026" />
+    <Book Series="Ultimate Endgame" Volume="2025" Number="4" Year="2026" />
+    <Book Series="Ultimate Endgame" Volume="2025" Number="5" Year="2026" />
+    <Book Series="Ultimate Universe Finale" Volume="2026" Number="1" Year="2026" />
+  </Books>
+</ReadingList>
