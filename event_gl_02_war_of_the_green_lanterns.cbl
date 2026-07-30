89f64e245811ee62de4c42fdc96193431e0942f3
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-07-30 23:38:07 +0100

    Add reading list for 'War of the Green Lanterns'

---
+++ event_gl_02_war_of_the_green_lanterns.cbl
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList Name="Event - GL 02 - War of the Green Lanterns">
+  <Name>Event - GL 02 - War of the Green Lanterns</Name>
+  <Books>
+    <Book Series="Green Lantern" Volume="2005" Number="63" Year="2011" />
+    <Book Series="Green Lantern Emerald Warriors" Volume="2010" Number="7" Year="2011" />
+    <Book Series="Green Lantern Corps" Volume="2006" Number="58" Year="2011" />
+    <Book Series="Green Lantern Emerald Warriors" Volume="2010" Number="8" Year="2011" />
+    <Book Series="Green Lantern" Volume="2005" Number="64" Year="2011" />
+    <Book Series="Green Lantern Corps" Volume="2006" Number="59" Year="2011" />
+    <Book Series="Green Lantern Emerald Warriors" Volume="2010" Number="9" Year="2011" />
+    <Book Series="Green Lantern" Volume="2005" Number="65" Year="2011" />
+    <Book Series="Green Lantern Corps" Volume="2006" Number="60" Year="2011" />
+    <Book Series="Green Lantern Emerald Warriors" Volume="2010" Number="10" Year="2011" />
+    <Book Series="Green Lantern" Volume="2005" Number="66" Year="2011" />
+    <Book Series="Green Lantern" Volume="2005" Number="67" Year="2011" />
+    <Book Series="War of the Green Lanterns Aftermath" Volume="2011" Number="1" Year="2011" />
+    <Book Series="War of the Green Lanterns Aftermath" Volume="2011" Number="2" Year="2011" />
+    <Book Series="Green Lantern Corps" Volume="2006" Number="61" Year="2011" />
+    <Book Series="Green Lantern Corps" Volume="2006" Number="62" Year="2011" />
+    <Book Series="Green Lantern Corps" Volume="2006" Number="63" Year="2011" />
+    <Book Series="Green Lantern Emerald Warriors" Volume="2010" Number="11" Year="2011" />
+    <Book Series="Green Lantern Emerald Warriors" Volume="2010" Number="12" Year="2011" />
+    <Book Series="Green Lantern Emerald Warriors" Volume="2010" Number="13" Year="2011" />
+  </Books>
+</ReadingList>
