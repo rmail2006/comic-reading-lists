fb7df00b2674c9da56109c551e98326854368444
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-07-30 23:31:52 +0100

    Update publication year for Larfleeze Christmas Special

--- event_gl_01_brightest_day_lantern_fallout.cbl
+++ event_gl_01_brightest_day_lantern_fallout.cbl
@@ -38,7 +38,7 @@
     <Book Series="Green Lantern Emerald Warriors" Volume="2010" Number="4" Year="2010" />
     <Book Series="Brightest Day" Volume="2010" Number="13" Year="2010" />
     <Book Series="Brightest Day" Volume="2010" Number="14" Year="2010" />
-    <Book Series="Green Lantern Larfleeze Christmas Special" Volume="2010" Number="1" Year="2010" />
+    <Book Series="Green Lantern Larfleeze Christmas Special" Volume="2011" Number="1" Year="2010" />
     <Book Series="Green Lantern" Volume="2005" Number="60" Year="2011" />
     <Book Series="Green Lantern Emerald Warriors" Volume="2010" Number="5" Year="2011" />
     <Book Series="Brightest Day" Volume="2010" Number="15" Year="2011" />
