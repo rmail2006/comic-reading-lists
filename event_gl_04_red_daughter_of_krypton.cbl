9d351727dd9d9842be284d7b66bbace9fdcc030d
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-07-31 10:05:45 +0100

    Create reading list for GL 04 - Red Daughter of Krypton
    Added a reading list for 'Event - GL 04 - Red Daughter of Krypton' with associated books.

---
+++ event_gl_04_red_daughter_of_krypton.cbl
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList Name="Event - GL 04 - Red Daughter of Krypton">
+  <Name>Event - GL 04 - Red Daughter of Krypton</Name>
+  <Books>
+    <Book Series="Supergirl" Volume="2011" Number="26" Year="2014" />
+    <Book Series="Supergirl" Volume="2011" Number="27" Year="2014" />
+    <Book Series="Red Lanterns" Volume="2011" Number="27" Year="2014" />
+    <Book Series="Green Lantern" Volume="2011" Number="28" Year="2014" />
+    <Book Series="Red Lanterns" Volume="2011" Number="28" Year="2014" />
+    <Book Series="Supergirl" Volume="2011" Number="28" Year="2014" />
+    <Book Series="Supergirl" Volume="2011" Number="29" Year="2014" />
+    <Book Series="Green Lantern" Volume="2011" Number="29" Year="2014" />
+    <Book Series="Red Lanterns" Volume="2011" Number="29" Year="2014" />
+    <Book Series="Supergirl" Volume="2011" Number="30" Year="2014" />
+    <Book Series="Red Lanterns" Volume="2011" Number="30" Year="2014" />
+    <Book Series="Supergirl" Volume="2011" Number="31" Year="2014" />
+    <Book Series="Red Lanterns" Volume="2011" Number="31" Year="2014" />
+    <Book Series="Supergirl" Volume="2011" Number="32" Year="2014" />
+    <Book Series="Red Lanterns" Volume="2011" Number="32" Year="2014" />
+    <Book Series="Supergirl" Volume="2011" Number="33" Year="2014" />
+    <Book Series="Red Lanterns" Volume="2011" Number="33" Year="2014" />
+    <Book Series="Red Lanterns Annual" Volume="2014" Number="1" Year="2014" />
+    <Book Series="Red Lanterns" Volume="2011" Number="34" Year="2014" />
+  </Books>
+</ReadingList>
