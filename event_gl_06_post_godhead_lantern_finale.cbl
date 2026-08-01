741d9be8f605bee36835f21c12d5f3d4504cd256
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-08-01 21:36:40 +0100

    Add XML reading list for Green Lantern events

---
+++ event_gl_06_post_godhead_lantern_finale.cbl
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList Name="Event - GL 06 - Post-Godhead Lantern Finale">
+  <Name>Event - GL 06 - Post-Godhead Lantern Finale</Name>
+  <Books>
+    <Book Series="Green Lantern" Volume="2011" Number="38" Year="2015" />
+    <Book Series="Green Lantern Corps" Volume="2011" Number="38" Year="2015" />
+    <Book Series="Green Lantern New Guardians" Volume="2011" Number="38" Year="2015" />
+    <Book Series="Red Lanterns" Volume="2011" Number="38" Year="2015" />
+    <Book Series="Sinestro" Volume="2014" Number="9" Year="2015" />
+    <Book Series="Green Lantern" Volume="2011" Number="39" Year="2015" />
+    <Book Series="Green Lantern Corps" Volume="2011" Number="39" Year="2015" />
+    <Book Series="Green Lantern New Guardians" Volume="2011" Number="39" Year="2015" />
+    <Book Series="Red Lanterns" Volume="2011" Number="39" Year="2015" />
+    <Book Series="Sinestro" Volume="2014" Number="10" Year="2015" />
+    <Book Series="Green Lantern" Volume="2011" Number="40" Year="2015" />
+    <Book Series="Green Lantern Corps" Volume="2011" Number="40" Year="2015" />
+    <Book Series="Green Lantern New Guardians" Volume="2011" Number="40" Year="2015" />
+    <Book Series="Red Lanterns" Volume="2011" Number="40" Year="2015" />
+    <Book Series="Red Lanterns Futures End" Volume="2014" Number="1" Year="2014" />
+    <Book Series="Sinestro" Volume="2014" Number="11" Year="2015" />
+    <Book Series="Sinestro Annual" Volume="2015" Number="1" Year="2015" />
+    <Book Series="Justice League The Darkseid War Green Lantern" Volume="2015" Number="1" Year="2015" />
+  </Books>
+</ReadingList>
