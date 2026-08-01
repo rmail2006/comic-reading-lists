47332a5fe9c9f10f258e0306c10b1af6159e1744
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-08-01 21:41:02 +0100

    Add XML reading list for GL 07 Convergence Lantern

---
+++ event_gl_07_convergence_lantern_tie_ins.cbl
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList Name="Event - GL 07 - Convergence Lantern Tie-Ins">
+  <Name>Event - GL 07 - Convergence Lantern Tie-Ins</Name>
+  <Books>
+    <Book Series="Convergence" Volume="2015" Number="0" Year="2015" />
+    <Book Series="Convergence" Volume="2015" Number="1" Year="2015" />
+    <Book Series="Convergence Green Lantern Parallax" Volume="2015" Number="1" Year="2015" />
+    <Book Series="Convergence Green Lantern Corps" Volume="2015" Number="1" Year="2015" />
+    <Book Series="Convergence" Volume="2015" Number="2" Year="2015" />
+    <Book Series="Convergence" Volume="2015" Number="3" Year="2015" />
+    <Book Series="Convergence" Volume="2015" Number="4" Year="2015" />
+    <Book Series="Convergence" Volume="2015" Number="5" Year="2015" />
+    <Book Series="Convergence Green Lantern Parallax" Volume="2015" Number="2" Year="2015" />
+    <Book Series="Convergence Green Lantern Corps" Volume="2015" Number="2" Year="2015" />
+    <Book Series="Convergence" Volume="2015" Number="6" Year="2015" />
+    <Book Series="Convergence" Volume="2015" Number="7" Year="2015" />
+    <Book Series="Convergence" Volume="2015" Number="8" Year="2015" />
+  </Books>
+</ReadingList>
