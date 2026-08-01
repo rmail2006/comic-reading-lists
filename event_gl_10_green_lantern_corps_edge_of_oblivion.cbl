aaedc5fb33564633e7fbc48a6978c91c1864a4be
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-08-01 21:52:14 +0100

    Add reading list for Green Lantern Corps Edge of Oblivion

---
+++ event_gl_10_green_lantern_corps_edge_of_oblivion.cbl
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList Name="Event - GL 10 - Green Lantern Corps Edge of Oblivion">
+  <Name>Event - GL 10 - Green Lantern Corps Edge of Oblivion</Name>
+  <Books>
+    <Book Series="Green Lantern Corps Edge of Oblivion" Volume="2016" Number="1" Year="2016" />
+    <Book Series="Green Lantern Corps Edge of Oblivion" Volume="2016" Number="2" Year="2016" />
+    <Book Series="Green Lantern Corps Edge of Oblivion" Volume="2016" Number="3" Year="2016" />
+    <Book Series="Green Lantern Corps Edge of Oblivion" Volume="2016" Number="4" Year="2016" />
+    <Book Series="Green Lantern Corps Edge of Oblivion" Volume="2016" Number="5" Year="2016" />
+    <Book Series="Green Lantern Corps Edge of Oblivion" Volume="2016" Number="6" Year="2016" />
+  </Books>
+</ReadingList>
