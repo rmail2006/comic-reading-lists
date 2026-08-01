e2c3cd0eda188d56fa218ff78884dad3b4d420ae
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-08-01 21:48:46 +0100

    Add XML reading list for Lobo and Sinestro series

---
+++ omnibus_gl_09_lobo_and_sinestro_finale.cbl
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList Name="Omnibus - GL 09 - Lobo and Sinestro Finale">
+  <Name>Omnibus - GL 09 - Lobo and Sinestro Finale</Name>
+  <Books>
+    <Book Series="Lobo" Volume="2014" Number="1" Year="2014" />
+    <Book Series="Lobo" Volume="2014" Number="2" Year="2014" />
+    <Book Series="Lobo" Volume="2014" Number="3" Year="2014" />
+    <Book Series="Lobo" Volume="2014" Number="4" Year="2015" />
+    <Book Series="Lobo" Volume="2014" Number="5" Year="2015" />
+    <Book Series="Lobo" Volume="2014" Number="6" Year="2015" />
+    <Book Series="Lobo" Volume="2014" Number="7" Year="2015" />
+    <Book Series="Lobo" Volume="2014" Number="8" Year="2015" />
+    <Book Series="Lobo" Volume="2014" Number="9" Year="2015" />
+    <Book Series="Sinestro" Volume="2014" Number="12" Year="2015" />
+    <Book Series="Sinestro" Volume="2014" Number="13" Year="2015" />
+    <Book Series="Sinestro" Volume="2014" Number="14" Year="2015" />
+    <Book Series="Lobo Annual" Volume="2015" Number="1" Year="2015" />
+    <Book Series="Lobo" Volume="2014" Number="10" Year="2015" />
+    <Book Series="Lobo" Volume="2014" Number="11" Year="2015" />
+    <Book Series="Sinestro" Volume="2014" Number="15" Year="2015" />
+    <Book Series="Sinestro" Volume="2014" Number="16" Year="2015" />
+    <Book Series="Sinestro" Volume="2014" Number="17" Year="2015" />
+    <Book Series="Sinestro" Volume="2014" Number="18" Year="2015" />
+    <Book Series="Sinestro" Volume="2014" Number="19" Year="2016" />
+    <Book Series="Sinestro" Volume="2014" Number="20" Year="2016" />
+    <Book Series="Sinestro" Volume="2014" Number="21" Year="2016" />
+    <Book Series="Sinestro" Volume="2014" Number="22" Year="2016" />
+    <Book Series="Sinestro" Volume="2014" Number="23" Year="2016" />
+    <Book Series="Lobo" Volume="2014" Number="12" Year="2015" />
+    <Book Series="Lobo" Volume="2014" Number="13" Year="2015" />
+  </Books>
+</ReadingList>
