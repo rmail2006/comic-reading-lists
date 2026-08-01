f40f161addac13c90ad8f893cfbca7f39e34ddd7
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-08-01 21:33:07 +0100

    Add XML reading list for Green Lantern Godhead event

---
+++ event_gl_05_green_lantern_new_gods_godhead.cbl
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList Name="Event - GL 05 - Green Lantern New Gods - Godhead">
+  <Name>Event - GL 05 - Green Lantern New Gods - Godhead</Name>
+  <Books>
+    <Book Series="Green Lantern New Gods Godhead" Volume="2014" Number="1" Year="2014" />
+    <Book Series="Green Lantern" Volume="2011" Number="35" Year="2014" />
+    <Book Series="Green Lantern Corps" Volume="2011" Number="35" Year="2014" />
+    <Book Series="Green Lantern New Guardians" Volume="2011" Number="35" Year="2014" />
+    <Book Series="Red Lanterns" Volume="2011" Number="35" Year="2014" />
+    <Book Series="Sinestro" Volume="2014" Number="6" Year="2014" />
+    <Book Series="Infinity Man and the Forever People" Volume="2014" Number="5" Year="2014" />
+    <Book Series="Green Lantern" Volume="2011" Number="36" Year="2014" />
+    <Book Series="Green Lantern Corps" Volume="2011" Number="36" Year="2014" />
+    <Book Series="Green Lantern New Guardians" Volume="2011" Number="36" Year="2014" />
+    <Book Series="Red Lanterns" Volume="2011" Number="36" Year="2014" />
+    <Book Series="Sinestro" Volume="2014" Number="7" Year="2014" />
+    <Book Series="Infinity Man and the Forever People" Volume="2014" Number="6" Year="2014" />
+    <Book Series="Green Lantern" Volume="2011" Number="37" Year="2014" />
+    <Book Series="Green Lantern Corps" Volume="2011" Number="37" Year="2014" />
+    <Book Series="Green Lantern New Guardians" Volume="2011" Number="37" Year="2014" />
+    <Book Series="Red Lanterns" Volume="2011" Number="37" Year="2014" />
+    <Book Series="Sinestro" Volume="2014" Number="8" Year="2014" />
+    <Book Series="Green Lantern Annual" Volume="2012" Number="3" Year="2014" />
+  </Books>
+</ReadingList>
