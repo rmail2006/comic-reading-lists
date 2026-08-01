37329115ab74bb74262e953e7ac1477629c44d0c
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-08-01 21:41:38 +0100

    Add reading list for Green Lantern Omnibus GL 08

---
+++ omnibus_gl_08_green_lantern_renegade_and_lost_army.cbl
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList Name="Omnibus - GL 08 - Green Lantern Renegade and Lost Army">
+  <Name>Omnibus - GL 08 - Green Lantern Renegade and Lost Army</Name>
+  <Books>
+    <Book Series="DC Sneak Peek Green Lantern" Volume="2015" Number="1" Year="2015" />
+    <Book Series="Green Lantern" Volume="2011" Number="41" Year="2015" />
+    <Book Series="Green Lantern" Volume="2011" Number="42" Year="2015" />
+    <Book Series="Green Lantern" Volume="2011" Number="43" Year="2015" />
+    <Book Series="Green Lantern" Volume="2011" Number="44" Year="2015" />
+    <Book Series="Green Lantern Annual" Volume="2012" Number="4" Year="2015" />
+    <Book Series="Green Lantern" Volume="2011" Number="45" Year="2015" />
+    <Book Series="Green Lantern" Volume="2011" Number="46" Year="2015" />
+    <Book Series="DC Sneak Peek Green Lantern The Lost Army" Volume="2015" Number="1" Year="2015" />
+    <Book Series="Green Lantern The Lost Army" Volume="2015" Number="1" Year="2015" />
+    <Book Series="Green Lantern The Lost Army" Volume="2015" Number="2" Year="2015" />
+    <Book Series="Green Lantern The Lost Army" Volume="2015" Number="3" Year="2015" />
+    <Book Series="Green Lantern The Lost Army" Volume="2015" Number="4" Year="2015" />
+    <Book Series="Green Lantern The Lost Army" Volume="2015" Number="5" Year="2015" />
+    <Book Series="Green Lantern The Lost Army" Volume="2015" Number="6" Year="2015" />
+    <Book Series="Green Lantern" Volume="2011" Number="47" Year="2015" />
+    <Book Series="Green Lantern" Volume="2011" Number="48" Year="2016" />
+    <Book Series="Green Lantern" Volume="2011" Number="49" Year="2016" />
+    <Book Series="Green Lantern" Volume="2011" Number="50" Year="2016" />
+    <Book Series="Green Lantern" Volume="2011" Number="51" Year="2016" />
+    <Book Series="Green Lantern" Volume="2011" Number="52" Year="2016" />
+  </Books>
+</ReadingList>
