195d2e3f1fb07b1115ea020f3209187a3ecbd4aa
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-08-01 21:55:19 +0100

    Add XML reading list for 'The Omega Men' series

---
+++ optional_gl_11_the_omega_men.cbl
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList Name="Optional - GL 11 - The Omega Men">
+  <Name>Optional - GL 11 - The Omega Men</Name>
+  <Books>
+    <Book Series="The Omega Men" Volume="2015" Number="1" Year="2015" />
+    <Book Series="The Omega Men" Volume="2015" Number="2" Year="2015" />
+    <Book Series="The Omega Men" Volume="2015" Number="3" Year="2015" />
+    <Book Series="The Omega Men" Volume="2015" Number="4" Year="2015" />
+    <Book Series="The Omega Men" Volume="2015" Number="5" Year="2015" />
+    <Book Series="The Omega Men" Volume="2015" Number="6" Year="2015" />
+    <Book Series="The Omega Men" Volume="2015" Number="7" Year="2015" />
+    <Book Series="The Omega Men" Volume="2015" Number="8" Year="2016" />
+    <Book Series="The Omega Men" Volume="2015" Number="9" Year="2016" />
+    <Book Series="The Omega Men" Volume="2015" Number="10" Year="2016" />
+    <Book Series="The Omega Men" Volume="2015" Number="11" Year="2016" />
+    <Book Series="The Omega Men" Volume="2015" Number="12" Year="2016" />
+  </Books>
+</ReadingList>
