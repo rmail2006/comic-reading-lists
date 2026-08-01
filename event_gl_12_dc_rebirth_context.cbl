3e7993684d6f9fdd6ef1d182730cdbb6fff3322f
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-08-02 00:29:01 +0100

    Create event_gl_12_dc_rebirth_context.cbl

---
+++ event_gl_12_dc_rebirth_context.cbl
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList Name="Event - DC Rebirth">
+  <Name>Event - DC Rebirth</Name>
+  <Books>
+    <Book Series="DC Universe Rebirth" Volume="2016" Number="1" Year="2016" />
+    <Book Series="The Flash Rebirth" Volume="2016" Number="1" Year="2016" />
+    <Book Series="The Flash" Volume="2016" Number="1" Year="2016" />
+    <Book Series="The Flash" Volume="2016" Number="2" Year="2016" />
+    <Book Series="The Flash" Volume="2016" Number="3" Year="2016" />
+    <Book Series="The Flash" Volume="2016" Number="4" Year="2016" />
+    <Book Series="The Flash" Volume="2016" Number="5" Year="2016" />
+    <Book Series="The Flash" Volume="2016" Number="6" Year="2016" />
+    <Book Series="The Flash" Volume="2016" Number="7" Year="2016" />
+    <Book Series="The Flash" Volume="2016" Number="8" Year="2016" />
+    <Book Series="Titans Rebirth" Volume="2016" Number="1" Year="2016" />
+    <Book Series="Titans" Volume="2016" Number="1" Year="2016" />
+    <Book Series="Titans" Volume="2016" Number="2" Year="2016" />
+    <Book Series="Titans" Volume="2016" Number="3" Year="2016" />
+    <Book Series="Titans" Volume="2016" Number="4" Year="2016" />
+    <Book Series="Titans" Volume="2016" Number="5" Year="2016" />
+    <Book Series="Titans" Volume="2016" Number="6" Year="2016" />
+    <Book Series="Superman Rebirth" Volume="2016" Number="1" Year="2016" />
+    <Book Series="Superman" Volume="2016" Number="1" Year="2016" />
+    <Book Series="Superman" Volume="2016" Number="2" Year="2016" />
+    <Book Series="Superman" Volume="2016" Number="3" Year="2016" />
+    <Book Series="Superman" Volume="2016" Number="4" Year="2016" />
+    <Book Series="Superman" Volume="2016" Number="5" Year="2016" />
+    <Book Series="Superman" Volume="2016" Number="6" Year="2016" />
+    <Book Series="Action Comics" Volume="2016" Number="957" Year="2016" />
+    <Book Series="Action Comics" Volume="2016" Number="958" Year="2016" />
+    <Book Series="Action Comics" Volume="2016" Number="959" Year="2016" />
+    <Book Series="Action Comics" Volume="2016" Number="960" Year="2016" />
+    <Book Series="Action Comics" Volume="2016" Number="961" Year="2016" />
+    <Book Series="Action Comics" Volume="2016" Number="962" Year="2016" />
+  </Books>
+</ReadingList>
