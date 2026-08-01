1097f82bfce0462d111ba6e7b422345caf565663
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-08-02 00:55:49 +0100

    Fix missing closing tag for ReadingList

---
+++ omnibus_gl_13_dc_rebirth_lantern_opening.cbl
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList Name="Omnibus - GL 13 - DC Rebirth Lantern Opening">
+  <Name>Omnibus - GL 13 - DC Rebirth Lantern Opening</Name>
+  <Books>
+    <Book Series="Green Lanterns Rebirth" Volume="2016" Number="1" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="1" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="2" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="3" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="4" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="5" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="6" Year="2016" />
+    <Book Series="Hal Jordan and the Green Lantern Corps Rebirth" Volume="2016" Number="1" Year="2016" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="1" Year="2016" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="2" Year="2016" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="3" Year="2016" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="4" Year="2016" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="5" Year="2016" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="6" Year="2016" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="7" Year="2016" />
+  </Books>
+</ReadingList>
