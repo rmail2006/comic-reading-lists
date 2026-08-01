a273ca722b62188a63c9efd5d713a38a9261d178
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-08-01 20:41:13 +0100

    Add XML reading list for Sinestro setup

---
+++ event_gl_04.5_sinestro_setup.cbl
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList Name="Event - GL 04.5 - Sinestro Setup">
+  <Name>Event - GL 04.5 - Sinestro Setup</Name>
+  <Books>
+    <Book Series="Sinestro" Volume="2014" Number="1" Year="2014" />
+    <Book Series="Sinestro" Volume="2014" Number="2" Year="2014" />
+    <Book Series="Sinestro" Volume="2014" Number="3" Year="2014" />
+    <Book Series="Sinestro" Volume="2014" Number="4" Year="2014" />
+    <Book Series="Sinestro" Volume="2014" Number="5" Year="2014" />
+  </Books>
+</ReadingList>
