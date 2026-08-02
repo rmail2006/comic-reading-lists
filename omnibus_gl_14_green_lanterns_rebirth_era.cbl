5439d25f86853c13820ee32b95444b4abb011f73
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-08-02 11:53:12 +0100

    Create reading list for Green Lanterns Rebirth Era
    Added a reading list for the Green Lanterns Rebirth Era, including volumes and numbers for each book in the series.

---
+++ omnibus_gl_14_green_lanterns_rebirth_era.cbl
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList Name="Omnibus - GL 14 - Green Lanterns Rebirth Era">
+  <Name>Omnibus - GL 14 - Green Lanterns Rebirth Era</Name>
+  <Books>
+    <Book Series="Green Lanterns" Volume="2016" Number="7" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="8" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="9" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="10" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="11" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="12" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="13" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="14" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="15" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="16" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="17" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="18" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="19" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="20" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="21" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="22" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="23" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="24" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="25" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="26" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="27" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="28" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="29" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="30" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="31" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="32" Year="2016" />
+  </Books>
+</ReadingList>
