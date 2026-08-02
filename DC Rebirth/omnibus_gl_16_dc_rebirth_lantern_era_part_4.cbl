897ec494a79255153c3a5b77337f24f7306ddf3e
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-08-02 12:02:55 +0100

    Create omnibus_gl_16_dc_rebirth_lantern_era_part_4.cbl

---
+++ DC Rebirth/omnibus_gl_16_dc_rebirth_lantern_era_part_4.cbl
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList Name="Omnibus - GL 16 - DC Rebirth Lantern Era Part 4">
+  <Name>Omnibus - GL 16 - DC Rebirth Lantern Era Part 4</Name>
+  <Books>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="22" Year="2016" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="23" Year="2016" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="24" Year="2016" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="25" Year="2016" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="26" Year="2016" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="27" Year="2016" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="28" Year="2016" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="29" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="22" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="23" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="24" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="25" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="26" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="27" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="28" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="29" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="30" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="31" Year="2016" />
+  </Books>
+</ReadingList>
