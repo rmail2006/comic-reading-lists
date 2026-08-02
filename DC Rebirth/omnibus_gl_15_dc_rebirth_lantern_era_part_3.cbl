8ac0c59831a4ced5fd4867de6a57f8a4d5e333c5
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-08-02 12:01:37 +0100

    Add reading list for GL 15 - DC Rebirth Lantern Era Part 3

---
+++ DC Rebirth/omnibus_gl_15_dc_rebirth_lantern_era_part_3.cbl
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList Name="Omnibus - GL 15 - DC Rebirth Lantern Era Part 3">
+  <Name>Omnibus - GL 15 - DC Rebirth Lantern Era Part 3</Name>
+  <Books>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="14" Year="2016" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="15" Year="2016" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="16" Year="2016" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="17" Year="2016" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="18" Year="2016" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="19" Year="2016" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="20" Year="2016" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="21" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="15" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="16" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="17" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="18" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="19" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="20" Year="2016" />
+    <Book Series="Green Lanterns" Volume="2016" Number="21" Year="2016" />
+  </Books>
+</ReadingList>
