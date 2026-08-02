064a77b8c33572b2c2398eca8010e24b228b44d8
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-08-02 12:06:27 +0100

    Update reading list for GL 17 Lantern Era Finale
    Updated the reading list to include additional volumes for both 'Hal Jordan and the Green Lantern Corps' and 'Green Lanterns' series, reflecting the finale of the DC Rebirth Lantern Era.

---
+++ DC Rebirth/omnibus_gl_17_dc_rebirth_lantern_era_finale.cbl
@@ -0,0 +1,61 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList Name="Omnibus - GL 17 - DC Rebirth Lantern Era Finale">
+  <Name>Omnibus - GL 17 - DC Rebirth Lantern Era Finale</Name>
+  <Books>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="30" Year="2016" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="31" Year="2016" />
+    <Book Series="Dark Nights: Batman The Dawnbreaker" Volume="2017" Number="31" Year="2017" />
+
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="32" Year="2017" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="33" Year="2018" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="34" Year="2018" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="35" Year="2018" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="36" Year="2018" />
+
+    <Book Series="Green Lanterns" Volume="2016" Number="32" Year="2017" />
+    <Book Series="Green Lanterns" Volume="2016" Number="33" Year="2018" />
+    <Book Series="Green Lanterns" Volume="2016" Number="34" Year="2018" />
+    <Book Series="Green Lanterns" Volume="2016" Number="35" Year="2018" />
+    <Book Series="Green Lanterns" Volume="2016" Number="36" Year="2018" />
+    <Book Series="Green Lanterns" Volume="2016" Number="37" Year="2018" />
+    <Book Series="Green Lanterns" Volume="2016" Number="38" Year="2018" />
+    <Book Series="Green Lanterns" Volume="2016" Number="39" Year="2018" />
+
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="37" Year="2018" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="38" Year="2018" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="39" Year="2018" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="40" Year="2018" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="41" Year="2018" />
+
+    <Book Series="Green Lanterns" Volume="2016" Number="40" Year="2018" />
+    <Book Series="Green Lanterns" Volume="2016" Number="41" Year="2018" />
+    <Book Series="Green Lanterns" Volume="2016" Number="42" Year="2018" />
+    <Book Series="Green Lanterns" Volume="2016" Number="43" Year="2018" />
+    <Book Series="Green Lanterns Annual" Volume="2018" Number="1" Year="2018" />
+    <Book Series="Green Lanterns" Volume="2016" Number="44" Year="2018" />
+    <Book Series="Green Lanterns" Volume="2016" Number="45" Year="2018" />
+    <Book Series="Green Lanterns" Volume="2016" Number="46" Year="2018" />
+    <Book Series="Green Lanterns" Volume="2016" Number="47" Year="2018" />
+
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="42" Year="2018" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="43" Year="2018" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="44" Year="2018" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="45" Year="2018" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="46" Year="2018" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="47" Year="2018" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="48" Year="2018" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="49" Year="2018" />
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="50" Year="2018" />
+
+    <Book Series="Green Lanterns" Volume="2016" Number="48" Year="2018" />
+    <Book Series="Green Lanterns" Volume="2016" Number="49" Year="2018" />
+    <Book Series="Green Lanterns" Volume="2016" Number="50" Year="2018" />
+    <Book Series="Green Lanterns" Volume="2016" Number="51" Year="2018" />
+    <Book Series="Green Lanterns" Volume="2016" Number="52" Year="2018" />
+    <Book Series="Green Lanterns" Volume="2016" Number="53" Year="2018" />
+    <Book Series="Green Lanterns" Volume="2016" Number="54" Year="2018" />
+    <Book Series="Green Lanterns" Volume="2016" Number="55" Year="2018" />
+    <Book Series="Green Lanterns" Volume="2016" Number="56" Year="2018" />
+    <Book Series="Green Lanterns" Volume="2016" Number="57" Year="2018" />
+  </Books>
+</ReadingList>
