c4d735cdcd375f21a49a3ed6b8ebc45ae87ecbc8
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-08-03 21:44:09 +0100

    Create omnibus_gl_18_morrisons_green_lantern_era.cbl

---
+++ omnibus_gl_18_morrisons_green_lantern_era.cbl
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList Name="Omnibus - GL 18 - Morrison Green Lantern Era">
+  <Name>Omnibus - GL 18 - Morrison Green Lantern Era</Name>
+  <Books>
+    <Book Series="The Green Lantern" Volume="2018" Number="1" Year="2018" />
+    <Book Series="The Green Lantern" Volume="2018" Number="2" Year="2018" />
+    <Book Series="The Green Lantern" Volume="2018" Number="3" Year="2019" />
+    <Book Series="The Green Lantern" Volume="2018" Number="4" Year="2019" />
+    <Book Series="The Green Lantern" Volume="2018" Number="5" Year="2019" />
+    <Book Series="The Green Lantern" Volume="2018" Number="6" Year="2019" />
+    <Book Series="The Green Lantern" Volume="2018" Number="7" Year="2019" />
+    <Book Series="The Green Lantern" Volume="2018" Number="8" Year="2019" />
+    <Book Series="The Green Lantern" Volume="2018" Number="9" Year="2019" />
+    <Book Series="The Green Lantern" Volume="2018" Number="10" Year="2019" />
+    <Book Series="The Green Lantern" Volume="2018" Number="11" Year="2019" />
+    <Book Series="The Green Lantern" Volume="2018" Number="12" Year="2019" />
+
+    <Book Series="The Green Lantern Annual" Volume="2019" Number="1" Year="2019" />
+
+    <Book Series="Green Lantern Blackstars" Volume="2019" Number="1" Year="2019" />
+    <Book Series="Green Lantern Blackstars" Volume="2019" Number="2" Year="2019" />
+    <Book Series="Green Lantern Blackstars" Volume="2019" Number="3" Year="2020" />
+
+    <Book Series="The Green Lantern Season Two" Volume="2020" Number="1" Year="2020" />
+    <Book Series="The Green Lantern Season Two" Volume="2020" Number="2" Year="2020" />
+    <Book Series="The Green Lantern Season Two" Volume="2020" Number="3" Year="2020" />
+    <Book Series="The Green Lantern Season Two" Volume="2020" Number="4" Year="2020" />
+    <Book Series="The Green Lantern Season Two" Volume="2020" Number="5" Year="2020" />
+    <Book Series="The Green Lantern Season Two" Volume="2020" Number="6" Year="2020" />
+    <Book Series="The Green Lantern Season Two" Volume="2020" Number="7" Year="2020" />
+    <Book Series="The Green Lantern Season Two" Volume="2020" Number="8" Year="2020" />
+    <Book Series="The Green Lantern Season Two" Volume="2020" Number="9" Year="2021" />
+    <Book Series="The Green Lantern Season Two" Volume="2020" Number="10" Year="2021" />
+    <Book Series="The Green Lantern Season Two" Volume="2020" Number="11" Year="2021" />
+    <Book Series="The Green Lantern Season Two" Volume="2020" Number="12" Year="2021" />
+
+    <Book Series="Green Lantern 80th Anniversary 100-Page Super Spectacular" Volume="2020" Number="1" Year="2020" />
+  </Books>
+</ReadingList>
