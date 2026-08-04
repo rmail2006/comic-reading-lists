f96ecc98d163ce7f151ea3f23f4f9e08367d743b
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-08-04 11:45:26 +0100

    Create omnibus_gl_era_01_brightest_day_to_flashpoint.cbl

---
+++ Green Lantern/omnibus_gl_era_01_brightest_day_to_flashpoint.cbl
@@ -0,0 +1,79 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList Name="Omnibus - GL Era 01 - Brightest Day to Flashpoint">
+  <Name>Omnibus - GL Era 01 - Brightest Day to Flashpoint</Name>
+  <Books>
+    <Book Series="Brightest Day" Volume="2010" Number="0" Year="2010" />
+    <Book Series="Brightest Day" Volume="2010" Number="1" Year="2010" />
+    <Book Series="Brightest Day" Volume="2010" Number="2" Year="2010" />
+    <Book Series="Green Lantern" Volume="2005" Number="53" Year="2010" />
+    <Book Series="Brightest Day" Volume="2010" Number="3" Year="2010" />
+    <Book Series="Brightest Day" Volume="2010" Number="4" Year="2010" />
+    <Book Series="Green Lantern" Volume="2005" Number="54" Year="2010" />
+    <Book Series="Green Lantern" Volume="2005" Number="55" Year="2010" />
+    <Book Series="Brightest Day" Volume="2010" Number="5" Year="2010" />
+    <Book Series="Brightest Day" Volume="2010" Number="6" Year="2010" />
+    <Book Series="Green Lantern" Volume="2005" Number="56" Year="2010" />
+    <Book Series="Green Lantern Emerald Warriors" Volume="2010" Number="1" Year="2010" />
+    <Book Series="Brightest Day" Volume="2010" Number="7" Year="2010" />
+    <Book Series="Brightest Day" Volume="2010" Number="8" Year="2010" />
+    <Book Series="Green Lantern" Volume="2005" Number="57" Year="2010" />
+    <Book Series="Green Lantern Emerald Warriors" Volume="2010" Number="2" Year="2010" />
+    <Book Series="Brightest Day" Volume="2010" Number="9" Year="2010" />
+    <Book Series="Brightest Day" Volume="2010" Number="10" Year="2010" />
+    <Book Series="Green Lantern" Volume="2005" Number="58" Year="2010" />
+    <Book Series="Green Lantern Emerald Warriors" Volume="2010" Number="3" Year="2010" />
+    <Book Series="Brightest Day" Volume="2010" Number="11" Year="2010" />
+    <Book Series="Brightest Day" Volume="2010" Number="12" Year="2010" />
+    <Book Series="Green Lantern" Volume="2005" Number="59" Year="2010" />
+    <Book Series="Green Lantern Corps" Volume="2006" Number="48" Year="2010" />
+    <Book Series="Green Lantern Corps" Volume="2006" Number="49" Year="2010" />
+    <Book Series="Green Lantern Corps" Volume="2006" Number="50" Year="2010" />
+    <Book Series="Green Lantern Corps" Volume="2006" Number="51" Year="2010" />
+    <Book Series="Green Lantern Corps" Volume="2006" Number="52" Year="2010" />
+    <Book Series="Green Lantern Corps" Volume="2006" Number="53" Year="2010" />
+    <Book Series="Green Lantern Corps" Volume="2006" Number="54" Year="2010" />
+    <Book Series="Green Lantern Corps" Volume="2006" Number="55" Year="2010" />
+    <Book Series="Green Lantern Corps" Volume="2006" Number="56" Year="2011" />
+    <Book Series="Green Lantern Corps" Volume="2006" Number="57" Year="2011" />
+    <Book Series="Green Lantern Emerald Warriors" Volume="2010" Number="4" Year="2010" />
+    <Book Series="Brightest Day" Volume="2010" Number="13" Year="2010" />
+    <Book Series="Brightest Day" Volume="2010" Number="14" Year="2010" />
+    <Book Series="Green Lantern Larfleeze Christmas Special" Volume="2011" Number="1" Year="2011" />
+    <Book Series="Green Lantern" Volume="2005" Number="60" Year="2011" />
+    <Book Series="Green Lantern Emerald Warriors" Volume="2010" Number="5" Year="2011" />
+    <Book Series="Brightest Day" Volume="2010" Number="15" Year="2011" />
+    <Book Series="Brightest Day" Volume="2010" Number="16" Year="2011" />
+    <Book Series="Green Lantern" Volume="2005" Number="61" Year="2011" />
+    <Book Series="Green Lantern Emerald Warriors" Volume="2010" Number="6" Year="2011" />
+    <Book Series="Brightest Day" Volume="2010" Number="17" Year="2011" />
+    <Book Series="Brightest Day" Volume="2010" Number="18" Year="2011" />
+    <Book Series="Green Lantern" Volume="2005" Number="62" Year="2011" />
+    <Book Series="Brightest Day" Volume="2010" Number="19" Year="2011" />
+    <Book Series="Brightest Day" Volume="2010" Number="20" Year="2011" />
+    <Book Series="Brightest Day" Volume="2010" Number="21" Year="2011" />
+    <Book Series="Brightest Day" Volume="2010" Number="22" Year="2011" />
+    <Book Series="Brightest Day" Volume="2010" Number="23" Year="2011" />
+    <Book Series="Brightest Day" Volume="2010" Number="24" Year="2011" />
+    <Book Series="Green Lantern Emerald Warriors" Volume="2010" Number="7" Year="2011" />
+    <Book Series="Green Lantern" Volume="2005" Number="63" Year="2011" />
+    <Book Series="Green Lantern Corps" Volume="2006" Number="58" Year="2011" />
+    <Book Series="Green Lantern Emerald Warriors" Volume="2010" Number="8" Year="2011" />
+    <Book Series="Green Lantern" Volume="2005" Number="64" Year="2011" />
+    <Book Series="Green Lantern Corps" Volume="2006" Number="59" Year="2011" />
+    <Book Series="Green Lantern Emerald Warriors" Volume="2010" Number="9" Year="2011" />
+    <Book Series="Green Lantern" Volume="2005" Number="65" Year="2011" />
+    <Book Series="Green Lantern Corps" Volume="2006" Number="60" Year="2011" />
+    <Book Series="Green Lantern Emerald Warriors" Volume="2010" Number="10" Year="2011" />
+    <Book Series="Green Lantern" Volume="2005" Number="66" Year="2011" />
+    <Book Series="Green Lantern" Volume="2005" Number="67" Year="2011" />
+    <Book Series="War of the Green Lanterns Aftermath" Volume="2011" Number="1" Year="2011" />
+    <Book Series="War of the Green Lanterns Aftermath" Volume="2011" Number="2" Year="2011" />
+    <Book Series="Green Lantern Corps" Volume="2006" Number="61" Year="2011" />
+    <Book Series="Green Lantern Corps" Volume="2006" Number="62" Year="2011" />
+    <Book Series="Green Lantern Corps" Volume="2006" Number="63" Year="2011" />
+    <Book Series="Green Lantern Emerald Warriors" Volume="2010" Number="11" Year="2011" />
+    <Book Series="Green Lantern Emerald Warriors" Volume="2010" Number="12" Year="2011" />
+    <Book Series="Green Lantern Emerald Warriors" Volume="2010" Number="13" Year="2011" />
+    <Book Series="Green Lantern Reading Notes" Volume="2026" Number="1" Year="2026" />
+  </Books>
+</ReadingList>
