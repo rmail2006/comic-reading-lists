6639113ff60a77743e1f1f7f8c68675b92ba8c28
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-08-04 12:35:24 +0100

    Create reading list for Green Lantern Era 03
    Added a comprehensive reading list for the Green Lantern series covering the era from 'Godhead' to 'Edge of Oblivion'.

---
+++ Green Lantern/omnibus_gl_era_03_godhead_to_edge_of_oblivion.cbl
@@ -0,0 +1,115 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList Name="Omnibus - GL Era 03 - Godhead to Edge of Oblivion">
+  <Name>Omnibus - GL Era 03 - Godhead to Edge of Oblivion</Name>
+  <Books>
+    <Book Series="Green Lantern Reading Notes" Volume="2026" Number="301" Year="2026"/>
+    <Book Series="Green Lantern New Gods Godhead" Volume="2014" Number="1" Year="2014"/>
+    <Book Series="Green Lantern" Volume="2011" Number="35" Year="2014"/>
+    <Book Series="Green Lantern Corps" Volume="2011" Number="35" Year="2014"/>
+    <Book Series="Green Lantern New Guardians" Volume="2011" Number="35" Year="2014"/>
+    <Book Series="Red Lanterns" Volume="2011" Number="35" Year="2014"/>
+    <Book Series="Sinestro" Volume="2014" Number="6" Year="2014"/>
+    <Book Series="Infinity Man and the Forever People" Volume="2014" Number="5" Year="2014"/>
+    <Book Series="Green Lantern" Volume="2011" Number="36" Year="2014"/>
+    <Book Series="Green Lantern Corps" Volume="2011" Number="36" Year="2014"/>
+    <Book Series="Green Lantern New Guardians" Volume="2011" Number="36" Year="2014"/>
+    <Book Series="Red Lanterns" Volume="2011" Number="36" Year="2014"/>
+    <Book Series="Sinestro" Volume="2014" Number="7" Year="2014"/>
+    <Book Series="Infinity Man and the Forever People" Volume="2014" Number="6" Year="2014"/>
+    <Book Series="Green Lantern" Volume="2011" Number="37" Year="2014"/>
+    <Book Series="Green Lantern Corps" Volume="2011" Number="37" Year="2014"/>
+    <Book Series="Green Lantern New Guardians" Volume="2011" Number="37" Year="2014"/>
+    <Book Series="Red Lanterns" Volume="2011" Number="37" Year="2014"/>
+    <Book Series="Sinestro" Volume="2014" Number="8" Year="2014"/>
+    <Book Series="Green Lantern Annual" Volume="2012" Number="3" Year="2014"/>
+    <Book Series="Green Lantern" Volume="2011" Number="38" Year="2015"/>
+    <Book Series="Green Lantern Corps" Volume="2011" Number="38" Year="2015"/>
+    <Book Series="Green Lantern New Guardians" Volume="2011" Number="38" Year="2015"/>
+    <Book Series="Red Lanterns" Volume="2011" Number="38" Year="2015"/>
+    <Book Series="Sinestro" Volume="2014" Number="9" Year="2015"/>
+    <Book Series="Green Lantern" Volume="2011" Number="39" Year="2015"/>
+    <Book Series="Green Lantern Corps" Volume="2011" Number="39" Year="2015"/>
+    <Book Series="Green Lantern New Guardians" Volume="2011" Number="39" Year="2015"/>
+    <Book Series="Red Lanterns" Volume="2011" Number="39" Year="2015"/>
+    <Book Series="Sinestro" Volume="2014" Number="10" Year="2015"/>
+    <Book Series="Green Lantern" Volume="2011" Number="40" Year="2015"/>
+    <Book Series="Green Lantern Corps" Volume="2011" Number="40" Year="2015"/>
+    <Book Series="Green Lantern New Guardians" Volume="2011" Number="40" Year="2015"/>
+    <Book Series="Red Lanterns" Volume="2011" Number="40" Year="2015"/>
+    <Book Series="Red Lanterns Futures End" Volume="2014" Number="1" Year="2014"/>
+    <Book Series="Sinestro" Volume="2014" Number="11" Year="2015"/>
+    <Book Series="Sinestro Annual" Volume="2015" Number="1" Year="2015"/>
+    <Book Series="Justice League The Darkseid War Green Lantern" Volume="2015" Number="1" Year="2015"/>
+    <Book Series="Green Lantern Reading Notes" Volume="2026" Number="302" Year="2026"/>
+    <Book Series="Convergence Green Lantern Parallax" Volume="2015" Number="1" Year="2015"/>
+    <Book Series="Convergence Green Lantern Corps" Volume="2015" Number="1" Year="2015"/>
+    <Book Series="Convergence Green Lantern Parallax" Volume="2015" Number="2" Year="2015"/>
+    <Book Series="Convergence Green Lantern Corps" Volume="2015" Number="2" Year="2015"/>
+    <Book Series="DC Sneak Peek Green Lantern" Volume="2015" Number="1" Year="2015"/>
+    <Book Series="Green Lantern" Volume="2011" Number="41" Year="2015"/>
+    <Book Series="Green Lantern" Volume="2011" Number="42" Year="2015"/>
+    <Book Series="Green Lantern" Volume="2011" Number="43" Year="2015"/>
+    <Book Series="Green Lantern" Volume="2011" Number="44" Year="2015"/>
+    <Book Series="Green Lantern Annual" Volume="2012" Number="4" Year="2015"/>
+    <Book Series="Green Lantern" Volume="2011" Number="45" Year="2015"/>
+    <Book Series="Green Lantern" Volume="2011" Number="46" Year="2015"/>
+    <Book Series="DC Sneak Peek Green Lantern The Lost Army" Volume="2015" Number="1" Year="2015"/>
+    <Book Series="Green Lantern The Lost Army" Volume="2015" Number="1" Year="2015"/>
+    <Book Series="Green Lantern The Lost Army" Volume="2015" Number="2" Year="2015"/>
+    <Book Series="Green Lantern The Lost Army" Volume="2015" Number="3" Year="2015"/>
+    <Book Series="Green Lantern The Lost Army" Volume="2015" Number="4" Year="2015"/>
+    <Book Series="Green Lantern The Lost Army" Volume="2015" Number="5" Year="2015"/>
+    <Book Series="Green Lantern The Lost Army" Volume="2015" Number="6" Year="2015"/>
+    <Book Series="Green Lantern" Volume="2011" Number="47" Year="2015"/>
+    <Book Series="Green Lantern" Volume="2011" Number="48" Year="2016"/>
+    <Book Series="Green Lantern" Volume="2011" Number="49" Year="2016"/>
+    <Book Series="Green Lantern" Volume="2011" Number="50" Year="2016"/>
+    <Book Series="Green Lantern" Volume="2011" Number="51" Year="2016"/>
+    <Book Series="Green Lantern" Volume="2011" Number="52" Year="2016"/>
+    <Book Series="Lobo" Volume="2014" Number="1" Year="2014"/>
+    <Book Series="Lobo" Volume="2014" Number="2" Year="2014"/>
+    <Book Series="Lobo" Volume="2014" Number="3" Year="2015"/>
+    <Book Series="Lobo" Volume="2014" Number="4" Year="2015"/>
+    <Book Series="Lobo" Volume="2014" Number="5" Year="2015"/>
+    <Book Series="Lobo" Volume="2014" Number="6" Year="2015"/>
+    <Book Series="Lobo" Volume="2014" Number="7" Year="2015"/>
+    <Book Series="Lobo" Volume="2014" Number="8" Year="2015"/>
+    <Book Series="Lobo" Volume="2014" Number="9" Year="2015"/>
+    <Book Series="Sinestro" Volume="2014" Number="12" Year="2015"/>
+    <Book Series="Sinestro" Volume="2014" Number="13" Year="2015"/>
+    <Book Series="Sinestro" Volume="2014" Number="14" Year="2015"/>
+    <Book Series="Lobo Annual" Volume="2015" Number="1" Year="2015"/>
+    <Book Series="Lobo" Volume="2014" Number="10" Year="2015"/>
+    <Book Series="Lobo" Volume="2014" Number="11" Year="2015"/>
+    <Book Series="Sinestro" Volume="2014" Number="15" Year="2015"/>
+    <Book Series="Sinestro" Volume="2014" Number="16" Year="2015"/>
+    <Book Series="Sinestro" Volume="2014" Number="17" Year="2015"/>
+    <Book Series="Sinestro" Volume="2014" Number="18" Year="2015"/>
+    <Book Series="Sinestro" Volume="2014" Number="19" Year="2015"/>
+    <Book Series="Sinestro" Volume="2014" Number="20" Year="2016"/>
+    <Book Series="Sinestro" Volume="2014" Number="21" Year="2016"/>
+    <Book Series="Sinestro" Volume="2014" Number="22" Year="2016"/>
+    <Book Series="Sinestro" Volume="2014" Number="23" Year="2016"/>
+    <Book Series="Lobo" Volume="2014" Number="12" Year="2015"/>
+    <Book Series="Lobo" Volume="2014" Number="13" Year="2015"/>
+    <Book Series="Green Lantern Corps Edge of Oblivion" Volume="2016" Number="1" Year="2016"/>
+    <Book Series="Green Lantern Corps Edge of Oblivion" Volume="2016" Number="2" Year="2016"/>
+    <Book Series="Green Lantern Corps Edge of Oblivion" Volume="2016" Number="3" Year="2016"/>
+    <Book Series="Green Lantern Corps Edge of Oblivion" Volume="2016" Number="4" Year="2016"/>
+    <Book Series="Green Lantern Corps Edge of Oblivion" Volume="2016" Number="5" Year="2016"/>
+    <Book Series="Green Lantern Corps Edge of Oblivion" Volume="2016" Number="6" Year="2016"/>
+    <Book Series="Green Lantern Reading Notes" Volume="2026" Number="303" Year="2026"/>
+    <Book Series="The Omega Men" Volume="2015" Number="1" Year="2015"/>
+    <Book Series="The Omega Men" Volume="2015" Number="2" Year="2015"/>
+    <Book Series="The Omega Men" Volume="2015" Number="3" Year="2015"/>
+    <Book Series="The Omega Men" Volume="2015" Number="4" Year="2015"/>
+    <Book Series="The Omega Men" Volume="2015" Number="5" Year="2015"/>
+    <Book Series="The Omega Men" Volume="2015" Number="6" Year="2015"/>
+    <Book Series="The Omega Men" Volume="2015" Number="7" Year="2015"/>
+    <Book Series="The Omega Men" Volume="2015" Number="8" Year="2016"/>
+    <Book Series="The Omega Men" Volume="2015" Number="9" Year="2016"/>
+    <Book Series="The Omega Men" Volume="2015" Number="10" Year="2016"/>
+    <Book Series="The Omega Men" Volume="2015" Number="11" Year="2016"/>
+    <Book Series="The Omega Men" Volume="2015" Number="12" Year="2016"/>
+  </Books>
+</ReadingList>
