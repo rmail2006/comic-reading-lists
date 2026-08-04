db07424ead501eabd71f33c408ac29e27191554d
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-08-04 13:26:14 +0100

    Remove Far Sector book entries from reading list

---
+++ Green Lantern/omnibus_gl_era_05_morrison_and_far_sector.cbl
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList Name="Omnibus - GL Era 05 - Morrison and Far Sector">
+  <Name>Omnibus - GL Era 05 - Morrison and Far Sector</Name>
+  <Books>
+    <!-- Note #501: Reading Note - The Green Lantern (2018): Morrison's Hal Jordan run is weird cosmic sci-fi. -->
+    <Book Series="Green Lantern Reading Notes" Volume="2026" Number="501" Year="2026" />
+    <Book Series="The Green Lantern" Volume="2018" Number="1" Year="2018" />
+    <Book Series="The Green Lantern" Volume="2018" Number="2" Year="2018" />
+    <Book Series="The Green Lantern" Volume="2018" Number="3" Year="2019" />
+    <Book Series="The Green Lantern" Volume="2018" Number="4" Year="2019" />
+    <Book Series="The Green Lantern" Volume="2018" Number="5" Year="2019" />
+    <Book Series="The Green Lantern" Volume="2018" Number="6" Year="2019" />
+    <Book Series="The Green Lantern" Volume="2018" Number="7" Year="2019" />
+    <Book Series="The Green Lantern" Volume="2018" Number="8" Year="2019" />
+    <Book Series="The Green Lantern" Volume="2018" Number="9" Year="2019" />
+    <Book Series="The Green Lantern" Volume="2018" Number="10" Year="2019" />
+    <Book Series="The Green Lantern" Volume="2018" Number="11" Year="2019" />
+    <Book Series="The Green Lantern" Volume="2018" Number="12" Year="2019" />
+    <Book Series="The Green Lantern Annual" Volume="2019" Number="1" Year="2019" />
+    <Book Series="Green Lantern Blackstars" Volume="2019" Number="1" Year="2019" />
+    <Book Series="Green Lantern Blackstars" Volume="2019" Number="2" Year="2020" />
+    <Book Series="Green Lantern Blackstars" Volume="2019" Number="3" Year="2020" />
+    <Book Series="The Green Lantern Season Two" Volume="2020" Number="1" Year="2020" />
+    <Book Series="The Green Lantern Season Two" Volume="2020" Number="2" Year="2020" />
+    <Book Series="The Green Lantern Season Two" Volume="2020" Number="3" Year="2020" />
+    <Book Series="The Green Lantern Season Two" Volume="2020" Number="4" Year="2020" />
+    <Book Series="The Green Lantern Season Two" Volume="2020" Number="5" Year="2020" />
+    <Book Series="The Green Lantern Season Two" Volume="2020" Number="6" Year="2020" />
+    <Book Series="The Green Lantern Season Two" Volume="2020" Number="7" Year="2020" />
+    <Book Series="The Green Lantern Season Two" Volume="2020" Number="8" Year="2020" />
+    <Book Series="The Green Lantern Season Two" Volume="2020" Number="9" Year="2021" />
+    <Book Series="The Green Lantern Season Two" Volume="2020" Number="10" Year="2021" />
+    <Book Series="The Green Lantern Season Two" Volume="2020" Number="11" Year="2021" />
+    <Book Series="The Green Lantern Season Two" Volume="2020" Number="12" Year="2021" />
+    <Book Series="Green Lantern 80th Anniversary 100-Page Super Spectacular" Volume="2020" Number="1" Year="2020" />
+    <!-- Note #502: Reading Note - Far Sector (2019) #1-12: standalone Jo Mullein story before Infinite Frontier. -->
+    <Book Series="Green Lantern Reading Notes" Volume="2026" Number="502" Year="2026" />
+  </Books>
+</ReadingList>
