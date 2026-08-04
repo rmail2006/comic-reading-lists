c5ecee7c67acef3f82d284cee1ecdb22f37d5290
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-08-04 13:59:42 +0100

    Create reading list for Omnibus - GL Era 07
    Added a new reading list for the Omnibus - GL Era 07, including various Green Lantern series and notes.

---
+++ Green Lantern/omnibus_gl_era_07_dawn_of_dc_green_lantern_era.cbl
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList Name="Omnibus - GL Era 07 - Dawn of DC Green Lantern Era">
+  <Name>Omnibus - GL Era 07 - Dawn of DC Green Lantern Era</Name>
+  <Books>
+    <Book Series="Green Lantern" Volume="2023" Number="1" Year="2023" />
+    <Book Series="Green Lantern" Volume="2023" Number="2" Year="2023" />
+
+    <!-- Note #701: Optional Read - Knight Terrors (2023): First Blood #1, Knight Terrors #1-4, Night's End #1 before Knight Terrors Green Lantern for full event context. -->
+    <Book Series="Green Lantern Reading Notes" Volume="2026" Number="701" Year="2026" />
+
+    <Book Series="Knight Terrors Green Lantern" Volume="2023" Number="1" Year="2023" />
+    <Book Series="Knight Terrors Green Lantern" Volume="2023" Number="2" Year="2023" />
+    <Book Series="Green Lantern" Volume="2023" Number="3" Year="2023" />
+    <Book Series="Green Lantern" Volume="2023" Number="4" Year="2023" />
+    <Book Series="Green Lantern" Volume="2023" Number="5" Year="2023" />
+    <Book Series="Green Lantern" Volume="2023" Number="6" Year="2023" />
+    <Book Series="Green Lantern" Volume="2023" Number="7" Year="2024" />
+    <Book Series="Green Lantern" Volume="2023" Number="8" Year="2024" />
+    <Book Series="Green Lantern" Volume="2023" Number="9" Year="2024" />
+    <Book Series="Green Lantern" Volume="2023" Number="10" Year="2024" />
+    <Book Series="Green Lantern" Volume="2023" Number="11" Year="2024" />
+    <Book Series="Green Lantern" Volume="2023" Number="12" Year="2024" />
+
+    <!-- Note #702: Reading Note - Green Lantern War Journal (2023): John Stewart prologue appears in Green Lantern #1-3 backups. -->
+    <Book Series="Green Lantern Reading Notes" Volume="2026" Number="702" Year="2026" />
+
+    <Book Series="Green Lantern War Journal" Volume="2023" Number="1" Year="2023" />
+    <Book Series="Green Lantern War Journal" Volume="2023" Number="2" Year="2023" />
+    <Book Series="Green Lantern War Journal" Volume="2023" Number="3" Year="2023" />
+    <Book Series="Green Lantern War Journal" Volume="2023" Number="4" Year="2023" />
+    <Book Series="Green Lantern War Journal" Volume="2023" Number="5" Year="2024" />
+    <Book Series="Green Lantern War Journal" Volume="2023" Number="6" Year="2024" />
+    <Book Series="Green Lantern War Journal" Volume="2023" Number="7" Year="2024" />
+    <Book Series="Green Lantern War Journal" Volume="2023" Number="8" Year="2024" />
+    <Book Series="Green Lantern War Journal" Volume="2023" Number="9" Year="2024" />
+    <Book Series="Green Lantern War Journal" Volume="2023" Number="10" Year="2024" />
+    <Book Series="Green Lantern War Journal" Volume="2023" Number="11" Year="2024" />
+    <Book Series="Green Lantern War Journal" Volume="2023" Number="12" Year="2024" />
+    <Book Series="Green Lantern" Volume="2023" Number="13" Year="2024" />
+    <Book Series="Green Lantern" Volume="2023" Number="14" Year="2024" />
+    <Book Series="Green Lantern" Volume="2023" Number="15" Year="2024" />
+
+    <!-- Note #703: Optional Read - Absolute Power (2024): Ground Zero #1, Absolute Power #1-4, Task Force VII #1-7, Origins #1-3 for full event context. -->
+    <Book Series="Green Lantern Reading Notes" Volume="2026" Number="703" Year="2026" />
+
+    <Book Series="Absolute Power Task Force VII" Volume="2024" Number="3" Year="2024" />
+    <Book Series="Green Lantern" Volume="2023" Number="16" Year="2024" />
+    <Book Series="Green Lantern" Volume="2023" Number="17" Year="2024" />
+    <Book Series="Green Lantern" Volume="2023" Number="18" Year="2024" />
+    <Book Series="Green Lantern Civil Corps Special" Volume="2025" Number="1" Year="2025" />
+    <Book Series="Green Lantern Fractured Spectrum Special" Volume="2025" Number="1" Year="2025" />
+    <Book Series="Green Lantern" Volume="2023" Number="19" Year="2025" />
+    <Book Series="Green Lantern" Volume="2023" Number="20" Year="2025" />
+    <Book Series="Green Lantern" Volume="2023" Number="21" Year="2025" />
+    <Book Series="Green Lantern" Volume="2023" Number="22" Year="2025" />
+    <Book Series="Green Lantern" Volume="2023" Number="23" Year="2025" />
+    <Book Series="Green Lantern" Volume="2023" Number="24" Year="2025" />
+    <Book Series="Green Lantern Corps" Volume="2025" Number="1" Year="2025" />
+    <Book Series="Green Lantern Corps" Volume="2025" Number="2" Year="2025" />
+    <Book Series="Green Lantern Corps" Volume="2025" Number="3" Year="2025" />
+    <Book Series="Green Lantern Corps" Volume="2025" Number="4" Year="2025" />
+    <Book Series="Green Lantern Corps" Volume="2025" Number="5" Year="2025" />
+    <Book Series="Green Lantern Corps" Volume="2025" Number="6" Year="2025" />
+
+    <!-- Note #704: Reading Note - Starbreaker Supremacy: Green Lantern #25-27 interleaves with Green Lantern Corps #7-9. -->
+    <Book Series="Green Lantern Reading Notes" Volume="2026" Number="704" Year="2026" />
+
+    <Book Series="Green Lantern" Volume="2023" Number="25" Year="2025" />
+    <Book Series="Green Lantern Corps" Volume="2025" Number="7" Year="2025" />
+    <Book Series="Green Lantern" Volume="2023" Number="26" Year="2025" />
+    <Book Series="Green Lantern Corps" Volume="2025" Number="8" Year="2025" />
+    <Book Series="Green Lantern" Volume="2023" Number="27" Year="2025" />
+    <Book Series="Green Lantern Corps" Volume="2025" Number="9" Year="2025" />
+  </Books>
+</ReadingList>
