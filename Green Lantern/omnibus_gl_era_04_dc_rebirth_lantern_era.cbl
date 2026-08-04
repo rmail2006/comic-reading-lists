92c412590d208803bda92676429cec3c20a55cd2
rmail2006 <52932651+rmail2006@users.noreply.github.com>
2026-08-04 13:14:08 +0100

    Create reading list for Green Lantern DC Rebirth era
    Added a new reading list for the Green Lantern era, including various series and volumes from 2016 to 2018.

---
+++ Green Lantern/omnibus_gl_era_04_dc_rebirth_lantern_era.cbl
@@ -0,0 +1,120 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList Name="Omnibus - GL Era 04 - DC Rebirth Lantern Era">
+  <Name>Omnibus - GL Era 04 - DC Rebirth Lantern Era</Name>
+  <Books>
+    <Book Series="DC Universe Rebirth" Volume="2016" Number="1" Year="2016"/>
+    <Book Series="Green Lantern Reading Notes" Volume="2026" Number="401" Year="2026"/>
+    <Book Series="Green Lanterns Rebirth" Volume="2016" Number="1" Year="2016"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="1" Year="2016"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="2" Year="2016"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="3" Year="2016"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="4" Year="2016"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="5" Year="2016"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="6" Year="2016"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps Rebirth" Volume="2016" Number="1" Year="2016"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="1" Year="2016"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="2" Year="2016"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="3" Year="2016"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="4" Year="2016"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="5" Year="2016"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="6" Year="2016"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="7" Year="2016"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="8" Year="2016"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="9" Year="2016"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="10" Year="2016"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="11" Year="2016"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="12" Year="2016"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="13" Year="2016"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="7" Year="2016"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="8" Year="2016"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="9" Year="2016"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="10" Year="2016"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="11" Year="2016"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="12" Year="2016"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="13" Year="2016"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="14" Year="2016"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="14" Year="2017"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="15" Year="2017"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="16" Year="2017"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="17" Year="2017"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="18" Year="2017"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="19" Year="2017"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="20" Year="2017"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="21" Year="2017"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="15" Year="2017"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="16" Year="2017"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="17" Year="2017"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="18" Year="2017"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="19" Year="2017"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="20" Year="2017"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="21" Year="2017"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="22" Year="2017"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="23" Year="2017"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="24" Year="2017"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="25" Year="2017"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="26" Year="2017"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="27" Year="2017"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="28" Year="2017"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="29" Year="2017"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="22" Year="2017"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="23" Year="2017"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="24" Year="2017"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="25" Year="2017"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="26" Year="2017"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="27" Year="2017"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="28" Year="2017"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="29" Year="2017"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="30" Year="2017"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="31" Year="2017"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="30" Year="2017"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="31" Year="2017"/>
+    <Book Series="Green Lantern Reading Notes" Volume="2026" Number="402" Year="2026"/>
+    <Book Series="Dark Nights Batman The Dawnbreaker" Volume="2017" Number="1" Year="2017"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="32" Year="2017"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="33" Year="2018"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="34" Year="2018"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="35" Year="2018"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="36" Year="2018"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="32" Year="2017"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="33" Year="2018"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="34" Year="2018"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="35" Year="2018"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="36" Year="2018"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="37" Year="2018"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="38" Year="2018"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="39" Year="2018"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="37" Year="2018"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="38" Year="2018"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="39" Year="2018"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="40" Year="2018"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="41" Year="2018"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="40" Year="2018"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="41" Year="2018"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="42" Year="2018"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="43" Year="2018"/>
+    <Book Series="Green Lanterns Annual" Volume="2018" Number="1" Year="2018"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="44" Year="2018"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="45" Year="2018"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="46" Year="2018"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="47" Year="2018"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="42" Year="2018"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="43" Year="2018"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="44" Year="2018"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="45" Year="2018"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="46" Year="2018"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="47" Year="2018"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="48" Year="2018"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="49" Year="2018"/>
+    <Book Series="Hal Jordan and the Green Lantern Corps" Volume="2016" Number="50" Year="2018"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="48" Year="2018"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="49" Year="2018"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="50" Year="2018"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="51" Year="2018"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="52" Year="2018"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="53" Year="2018"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="54" Year="2018"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="55" Year="2018"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="56" Year="2018"/>
+    <Book Series="Green Lanterns" Volume="2016" Number="57" Year="2018"/>
+  </Books>
+</ReadingList>
